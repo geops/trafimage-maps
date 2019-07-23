ee0a80504c45a729bf12c006db809e8e7d0462fb
Mario Härtwig <mario.haertwig@geops.de>
2019-07-23 15:39:57 +0200

    Import ie11 polyfills in main component.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
