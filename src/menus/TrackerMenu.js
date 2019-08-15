37a3fa059e7ac6200fa831bec1700ef41e9e977c
lucien <lucien.edel@geops.de>
2019-08-15 17:22:14 +0200

    Add RouteSchedule component in train tracker menu

--- src/menus/TrackerMenu.js
+++ src/menus/TrackerMenu.js
@@ -4,6 +4,7 @@ import { TiVideo } from 'react-icons/ti';
 import Map from 'ol/Map';
 import LayerService from 'react-spatial/LayerService';
 import TrajservLayer from 'react-transit/layers/TrajservLayer';
+import RouteSchedule from 'react-transit/components/RouteSchedule';
 import MenuItem from '../components/Menu/MenuItem';
 import './TrackerMenu.scss';
 
@@ -39,31 +40,12 @@ class TrackerMenu extends Component {
         this.setState({ open: this.trackerLayer.getVisible() }),
       );
 
-      this.trackerLayer.onClick(trajectories => {
-        if (trajectories.length) {
-          const trajectory = [];
-          const id = trajectories[0].get('id');
-
-          this.trackerLayer.fetchTrajectory(id).then(traj => {
-            if (traj.p.length) {
-              this.setState({ open: true, collapsed: false });
-            }
-            for (let i = 0; i < traj.p.length; i += 1) {
-              for (let j = 0; j < traj.p[i].length; j += 1) {
-                const stop = traj.p[i][j];
-                if (stop.n) {
-                  trajectory.push({
-                    name: stop.n,
-                    arrival: stop.a ? stop.a * 1000 : null,
-                    departure: stop.d ? stop.d * 1000 : null,
-                  });
-                }
-              }
-            }
-
-            this.setState({ trajectory });
-          });
-        }
+      this.trackerLayer.onClick(traj => {
+        this.setState({
+          open: true,
+          collapsed: false,
+          trajectory: traj,
+        });
       });
     }
   }
@@ -71,26 +53,9 @@ class TrackerMenu extends Component {
   render() {
     const { open, collapsed, trajectory } = this.state;
     const { map } = this.props;
-    let trajectoryElem = null;
 
     if (!open) {
       return null;
-    }
-
-    if (trajectory) {
-      trajectoryElem = (
-        <div className="wkp-trajectory">
-          {trajectory.map(t => (
-            <div key={t.name} className="wkp-trajectory-stop">
-              <div className="wkp-trajectory-stop-time">
-                {TrackerMenu.getTimeString(new Date(t.arrival))}
-                <div className="wkp-trajectory-stop-icon" />
-              </div>
-              <div className="wkp-trajectory-stop-name">{t.name}</div>
-            </div>
-          ))}
-        </div>
-      );
     }
 
     return (
@@ -103,7 +68,7 @@ class TrackerMenu extends Component {
         collapsed={collapsed}
         onCollapseToggle={c => this.setState({ collapsed: c })}
       >
-        {trajectoryElem}
+        {trajectory ? <RouteSchedule lineInfos={trajectory} /> : null}
       </MenuItem>
     );
   }
