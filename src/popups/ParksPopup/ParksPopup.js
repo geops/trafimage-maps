068506f5ce4bcc5056a798fa07ba394dc8a9fe72
gevos <eva.schiller@geops.de>
2019-08-22 12:24:59 +0200

    Parks: preload images (#41)

--- src/popups/ParksPopup/ParksPopup.js
+++ src/popups/ParksPopup/ParksPopup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Feature from 'ol/Feature';
 import { withTranslation } from 'react-i18next';
@@ -12,54 +12,89 @@ const propTypes = {
   t: PropTypes.func.isRequired,
 };
 
-const ParksPopup = ({ feature, t }) => {
-  const parkName = feature.get('park_name');
-  const abk = feature.get('abk');
-  const parkTyp = feature.get('park_typ');
-  const webUrl = feature.get('web');
+class ParksPopup extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
 
-  let images;
-  let typ;
-  let web;
+  componentWillMount() {
+    const { feature } = this.props;
+    const abk = feature.get('abk');
 
-  if (abk) {
-    const parkImageUrl = `http://www.paerke.ch/bilder/kartenportal/foto_${abk.toLowerCase()}.jpg`;
-    const labelLogoUrl = `http://angebote.paerke.ch/export/iframe/label_logo/${abk}`;
-    const parkLogoUrl = `http://angebote.paerke.ch/export/iframe/park_logo/${abk}`;
+    if (abk) {
+      this.parkImageUrl = `http://www.paerke.ch/bilder/kartenportal/foto_${abk.toLowerCase()}.jpg`;
+      this.labelLogoUrl = `http://angebote.paerke.ch/export/iframe/label_logo/${abk}`;
+      this.parkLogoUrl = `http://angebote.paerke.ch/export/iframe/park_logo/${abk}`;
 
-    images = (
-      <>
-        <img src={parkImageUrl} alt={t('Bild des Parks')} />
-        <img src={labelLogoUrl} alt={t('Label des Parks')} />
-        <img src={parkLogoUrl} alt={t('Logo des Parks')} />
-      </>
+      this.preload([this.parkImageUrl, this.labelLogoUrl, this.parkLogoUrl]);
+    }
+  }
+
+  preload(urls) {
+    Promise.all(
+      urls.map(url => {
+        return new Promise(resolve => {
+          const image = new Image();
+          image.onload = resolve;
+          image.src = url;
+        });
+      }),
+    ).then(() => {
+      this.setState({ loaded: true });
+    });
+  }
+
+  render() {
+    const { feature, t } = this.props;
+    const { loaded } = this.state;
+
+    const parkName = feature.get('park_name');
+    const parkTyp = feature.get('park_typ');
+    const webUrl = feature.get('web');
+
+    let images;
+    let typ;
+    let web;
+
+    if (loaded) {
+      images = (
+        <>
+          <img src={this.parkImageUrl} alt={t('Bild des Parks')} />
+          <img src={this.labelLogoUrl} alt={t('Label des Parks')} />
+          <img src={this.parkLogoUrl} alt={t('Logo des Parks')} />
+        </>
+      );
+    }
+
+    if (parkTyp) {
+      typ = <div>{t(parkTyp)}</div>;
+    }
+
+    if (webUrl) {
+      const protocol = 'http://';
+      const url =
+        webUrl.indexOf(protocol) > -1 ? webUrl : `${protocol}${webUrl}`;
+
+      web = (
+        <a href={url} rel="noopener noreferrer" target="_blank">
+          {t('Webseite des Parks')}
+        </a>
+      );
+    }
+
+    return (
+      <div className="wkp-parks-popup">
+        <div className="wkp-parks-popup-title">{parkName}</div>
+        {images}
+        {typ}
+        {web}
+      </div>
     );
   }
-
-  if (parkTyp) {
-    typ = <div>{t(parkTyp)}</div>;
-  }
-
-  if (webUrl) {
-    const protocol = 'http://';
-    const url = webUrl.indexOf(protocol) > -1 ? webUrl : `${protocol}${webUrl}`;
-
-    web = (
-      <a href={url} rel="noopener noreferrer" target="_blank">
-        {t('Webseite des Parks')}
-      </a>
-    );
-  }
-
-  return (
-    <div className="wkp-parks-popup">
-      <div className="wkp-parks-popup-title">{parkName}</div>
-      {images}
-      {typ}
-      {web}
-    </div>
-  );
-};
+}
 
 const mapStateToProps = state => ({
   language: state.app.language,
