8294fba103f3bd33abbfe3018223826d9229c80c
Ben Everad <ben.everad@geops.de>
2019-09-04 16:26:20 +0200

    WIP Footer_iimpressum

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
@@ -10,6 +10,10 @@ import Copyright from 'react-spatial/components/Copyright';
 import Select from 'react-spatial/components/Select';
 import MousePosition from 'react-spatial/components/MousePosition';
 import LayerService from 'react-spatial/LayerService';
+import ActionLink from 'react-spatial/components/ActionLink';
+import Dialog from 'react-spatial/components/Dialog';
+
+import { FaInfo } from 'react-icons/fa';
 
 import { setLanguage, setProjection } from '../../model/app/actions';
 import './Footer.scss';
@@ -34,6 +38,62 @@ const numberFormat = coords => {
 
   return coordStr;
 };
+/*
+function langText(lang,value){
+  if (lang === "de"){
+    return({'Title':'Impressum','Text':'Dies ist ein deutsches Umprewssion'})
+  } else {
+    return {'Title':null,'Text':null}
+  }
+}
+
+class DialogFooter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: null,
+      inhalt: {'Title':null,'Text':null},
+    };
+  }
+
+  toggleDialog(value) {
+    const { isOpen } = this.state;
+    this.setState({
+      isOpen: !isOpen,
+      lang:this.props.lang,
+      inhalt: langText(this.props.lang,value),
+    });
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <div className="wkp-impressum">
+        <ActionLink onClick={() => this.toggleDialog('Kontakt')}>
+        Kontakt
+        </ActionLink>
+        <ActionLink onClick={() => this.toggleDialog('Impressum')}>
+        Impressum
+        </ActionLink>
+        <ActionLink onClick={() => this.toggleDialog('Rechtliches')}>
+        Rechtliches
+        </ActionLink>
+        
+        <Dialog
+          title={<span>{this.state.inhalt.Title}</span>}
+          classNameChildren="tm-dialog-content"
+          onClose={() => this.toggleDialog()}
+          cancelDraggable=".tm-dialog-content"
+          isOpen={isOpen}
+          isModal
+        >
+          <span>{this.state.inhalt.Text}</span>
+        </Dialog>
+      </div>
+    );
+  }
+}
+*/
 
 const Footer = ({
   map,
@@ -42,62 +102,105 @@ const Footer = ({
   t,
   dispatchSetLanguage,
   dispatchSetProjection,
-}) => (
-  <RSFooter className="wkp-footer">
-    <div className="wkp-footer-left">
-      <Copyright
-        layerService={layerService}
-        format={f => `${t('Geodaten')} ${f}`}
-      />
-    </div>
+}) => {
+  const [isOpen, setIsOpen] = useState(null);
 
-    <div className="wkp-footer-right">
-      <MousePosition
-        coordinatePosition="left"
-        map={map}
-        onChange={(evt, proj) => {
-          dispatchSetProjection(proj);
-        }}
-        projections={[
-          {
-            label: 'CH1093 / LV03',
-            value: 'EPSG:21781',
-            format: c => `${t('Koordinaten')}: ${numberFormat(c)}`,
-          },
-          {
-            label: 'CH1093+ / LV95',
-            value: 'EPSG:2056',
-            format: c => `${t('Koordinaten')}: ${numberFormat(c)}`,
-          },
-          {
-            label: 'Web Mercator',
-            value: 'EPSG:3857',
-            format: c => `${t('Koordinaten')}: ${numberFormat(c)}`,
-          },
-          {
-            label: 'WGS 84',
-            value: 'EPSG:4326',
-            format: c => `${t('Koordinaten')}: ${c}`,
-          },
-        ]}
-      />
-      <ScaleLine map={map} />
-      <Select
-        className="wkp-language-select"
-        options={[
-          { label: 'DE', value: 'de' },
-          { label: 'FR', value: 'fr' },
-          { label: 'IT', value: 'it' },
-          { label: 'EN', value: 'en' },
-        ]}
-        value={language}
-        onChange={(e, opt) => {
-          dispatchSetLanguage(opt.value);
-        }}
-      />
-    </div>
-  </RSFooter>
-);
+  const getDialogContent = value => {
+    if (value === 'Kontakt') {
+      return { __html: t('contact_content') };
+    }
+    if (value === 'Impressum') {
+      return t('impressum_content');
+    }
+    if (value === 'Rechtliches') {
+      return t('legal_content');
+    }
+    return null;
+  };
+  return (
+    <RSFooter className="wkp-footer">
+      <div className="wkp-footer-left">
+        <Copyright
+          layerService={layerService}
+          format={f => `${t('Geodaten')} ${f}`}
+        />
+
+        <div className="wkp-impressum">
+          <ActionLink onClick={() => setIsOpen('Kontakt')}>
+            {t('Kontakt')}
+          </ActionLink>
+          <ActionLink onClick={() => setIsOpen('Impressum')}>
+            {t('Impressum')}
+          </ActionLink>
+          <ActionLink onClick={() => setIsOpen('Rechtliches')}>
+            {t('Rechtliches')}
+          </ActionLink>
+
+          <Dialog
+            title={
+              <div>
+                <FaInfo /> {t(isOpen)}
+              </div>
+            }
+            classNameChildren="tm-dialog-content"
+            onClose={() => setIsOpen(null)}
+            cancelDraggable=".tm-dialog-content"
+            isOpen={isOpen}
+            isModal
+          >
+            <div dangerouslySetInnerHTML={getDialogContent(isOpen)} />
+          </Dialog>
+        </div>
+      </div>
+
+      <div className="wkp-footer-right">
+        <MousePosition
+          coordinatePosition="left"
+          map={map}
+          onChange={(evt, proj) => {
+            dispatchSetProjection(proj);
+          }}
+          projections={[
+            {
+              label: 'CH1093 / LV03',
+              value: 'EPSG:21781',
+              format: c => `${t('Koordinaten')}: ${numberFormat(c)}`,
+            },
+            {
+              label: 'CH1093+ / LV95',
+              value: 'EPSG:2056',
+              format: c => `${t('Koordinaten')}: ${numberFormat(c)}`,
+            },
+            {
+              label: 'Web Mercator',
+              value: 'EPSG:3857',
+              format: c => `${t('Koordinaten')}: ${numberFormat(c)}`,
+            },
+            {
+              label: 'WGS 84',
+              value: 'EPSG:4326',
+              format: c => `${t('Koordinaten')}: ${c}`,
+            },
+          ]}
+        />
+        <ScaleLine map={map} />
+        <Select
+          className="wkp-language-select"
+          options={[
+            { label: 'DE', value: 'de' },
+            { label: 'FR', value: 'fr' },
+            { label: 'IT', value: 'it' },
+            { label: 'EN', value: 'en' },
+          ]}
+          value={language}
+          onChange={(e, opt) => {
+            dispatchSetLanguage(opt.value);
+          }}
+        />
+      </div>
+    </RSFooter>
+  );
+};
 
 const mapStateToProps = state => ({
   language: state.app.language,
