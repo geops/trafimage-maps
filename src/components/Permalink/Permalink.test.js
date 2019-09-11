cc586740de09116ca1b13477bf92ce2017f6a54c
lucien <lucien.edel@geops.de>
2019-09-11 17:18:48 +0200

    remove comment

--- src/components/Permalink/Permalink.test.js
+++ src/components/Permalink/Permalink.test.js
@@ -5,22 +5,10 @@ import { configure, mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import Adapter from 'enzyme-adapter-react-16';
-
 import LayerService from 'react-spatial/LayerService';
 import Layer from 'react-spatial/layers/Layer';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-/*
-import MapEvent from 'ol/MapEvent';
-import OLMap from 'ol/Map';
-import View from 'ol/View';
-*/
-/*
-import data from '../../../data/TreeData';
-import data2 from '../../../data/ExampleData';
-import LayerService from '../../LayerService';
-import ConfigReader from '../../ConfigReader';
-*/
 import Permalink from './Permalink';
 
 configure({ adapter: new Adapter() });
