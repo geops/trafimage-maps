b0ab582af52021b536d607daacb2a3cda48b0850
Eva Schiller | geOps <eva.schiller@geops.de>
2019-10-24 11:31:19 +0200

    Replace lib querystring by query-string (and upgrade)

--- src/layers/NetzkartePointLayer/NetzkartePointLayer.js
+++ src/layers/NetzkartePointLayer/NetzkartePointLayer.js
@@ -1,4 +1,4 @@
-import qs from 'querystring';
+import qs from 'query-string';
 import VectorLayer from 'react-spatial/layers/VectorLayer';
 import OLVectorLayer from 'ol/layer/Vector';
 import OLVectorSource from 'ol/source/Vector';
