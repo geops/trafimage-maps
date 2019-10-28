df576331a26d829255bb3ce8afcb0718598a15d6
lucien <lucien.edel@geops.de>
2019-10-28 15:25:20 +0100

    Activate light and night netzkarte baselayers

--- src/config/layers.js
+++ src/config/layers.js
@@ -478,8 +478,8 @@ export const stuetzpunktbahnhoefe = new HandicapLayer({
 export default [
   sourcesLayer,
   netzkarteLayer,
-  // netzkarteLayerLight,
-  // netzkarteLayerNight,
+  netzkarteLayerLight,
+  netzkarteLayerNight,
   swisstopoLandeskarteGrau,
   swisstopoLandeskarte,
   aerial,
