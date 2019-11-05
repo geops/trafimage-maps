6b5810303e9b5124725cc1d2fdcf8764d3f4f049
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2019-11-05 14:31:05 +0100

    Move swissExtent variable to config because only used there.

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -8,8 +8,6 @@ let vectorTilesKey = '5cc87b12d7c5370001c1d6557f01e26728174c1fa19d33afe303b910';
 let vectorTilesUrl = '//maps.geops.io';
 let cartaroUrl = '//cartaro2.dev.trafimage.ch/api/v1/';
 
-// Swiss extent in 3857.
-const swissExtent = [656409.5, 5740863.4, 1200512.3, 6077033.16];
 const env = process && process.env && process.env.REACT_APP_ENV;
 
 switch (env) {
@@ -38,7 +36,6 @@ export default {
   geoadminWmtsUrl,
   geoserverUrl,
   geoserverCachedUrl,
-  swissExtent,
   tileserverUrlMapproxy,
   vectorTilesUrl,
   vectorTilesKey,
