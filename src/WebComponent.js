60e4248e0c4e2d218b6652c76a207f1d9b042827
Olivier Terral <olivier.terral@geops.de>
2019-11-08 12:28:44 +0100

    Use attributes

--- src/WebComponent.js
+++ src/WebComponent.js
@@ -1,5 +1,6 @@
+/* eslint-disable react/require-default-props */
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Styled } from 'create-react-web-component';
 import TrafimageMaps from './components/TrafimageMaps';
@@ -7,21 +8,25 @@ import styles from './WebComponent.scss';
 import defaultTopics from './config/topics';
 
 const propTypes = {
+  // Properties
   center: PropTypes.arrayOf(PropTypes.number),
-  zoom: PropTypes.number,
   topics: PropTypes.array,
-  elements: PropTypes.shape({
-    header: PropTypes.bool,
-    footer: PropTypes.bool,
-    menu: PropTypes.bool,
-    permaLink: PropTypes.bool,
-    popup: PropTypes.bool,
-    mapControls: PropTypes.bool,
-    baseLayerToggler: PropTypes.bool,
-    shareMenu: PropTypes.bool,
-    featureMenu: PropTypes.bool,
-    trackerMenu: PropTypes.bool,
-  }),
+
+  // Attributes
+  width: PropTypes.string,
+  height: PropTypes.string,
+  zoom: PropTypes.number,
+  header: PropTypes.string,
+  footer: PropTypes.string,
+  menu: PropTypes.string,
+  permalink: PropTypes.string,
+  search: PropTypes.string,
+  popup: PropTypes.string,
+  mapControls: PropTypes.string,
+  baseLayerToggler: PropTypes.string,
+  shareMenu: PropTypes.string,
+  featureMenu: PropTypes.string,
+  trackerMenu: PropTypes.string,
   apiKey: PropTypes.string,
   cartaroUrl: PropTypes.string,
   geoServerUrl: PropTypes.string,
@@ -29,23 +34,21 @@ const propTypes = {
   vectorTilesUrl: PropTypes.string,
 };
 
-const defaultProps = {
-  center: [925472, 5920000],
+const attributes = {
+  width: '100%',
+  height: '100%',
   zoom: undefined,
-  elements: {
-    footer: true,
-    header: true,
-    mapControls: true,
-    menu: true,
-    popup: true,
-    search: true,
-    permalink: false,
-    baseLayerToggler: true,
-    shareMenu: true,
-    featureMenu: false,
-    trackerMenu: true,
-  },
-  topics: defaultTopics,
+  footer: 'true',
+  header: 'true',
+  mapControls: 'true',
+  menu: 'true',
+  popup: 'true',
+  search: 'true',
+  permalink: 'false',
+  baseLayerToggler: 'true',
+  shareMenu: 'true',
+  featureMenu: 'false',
+  trackerMenu: 'true',
   apiKey: process.env.REACT_APP_VECTOR_TILES_KEY,
   cartaroUrl: process.env.REACT_APP_CARTARO_URL,
   geoServerUrl: process.env.REACT_APP_GEOSERVER_URL,
@@ -53,9 +56,63 @@ const defaultProps = {
   vectorTilesUrl: process.env.REACT_APP_VECTOR_TILES_URL,
 };
 
+const defaultProps = {
+  center: [925472, 5920000],
+  topics: defaultTopics,
+};
+
+const getBool = val => val === 'true' || val === true;
+
 const WebComponent = props => {
-  // eslint-disable-next-line react/prop-types
-  const { width, height } = props;
+  const {
+    width,
+    height,
+    zoom,
+    footer,
+    header,
+    mapControls,
+    menu,
+    popup,
+    search,
+    permalink,
+    baseLayerToggler,
+    shareMenu,
+    featureMenu,
+    trackerMenu,
+  } = props;
+
+  const floatZoom = useMemo(() => {
+    return parseFloat(zoom) || null;
+  }, [zoom]);
+
+  const boolElements = useMemo(() => {
+    return {
+      footer: getBool(footer),
+      header: getBool(header),
+      mapControls: getBool(mapControls),
+      menu: getBool(menu),
+      popup: getBool(popup),
+      search: getBool(search),
+      permalink: getBool(permalink),
+      baseLayerToggler: getBool(baseLayerToggler),
+      shareMenu: getBool(shareMenu),
+      featureMenu: getBool(featureMenu),
+      trackerMenu: getBool(trackerMenu),
+    };
+  }, [
+    footer,
+    header,
+    mapControls,
+    menu,
+    popup,
+    search,
+    permalink,
+    baseLayerToggler,
+    shareMenu,
+    featureMenu,
+    trackerMenu,
+  ]);
+
   return (
     <Styled styles={styles}>
       <div
@@ -65,7 +122,12 @@ const WebComponent = props => {
           height,
         }}
       >
-        <TrafimageMaps {...props} />
+        <TrafimageMaps
+          {...props}
+          elements={boolElements}
+          initialZoom={floatZoom}
+          zoom={floatZoom}
+        />
       </div>
     </Styled>
   );
@@ -73,9 +135,6 @@ const WebComponent = props => {
 
 WebComponent.propTypes = propTypes;
 WebComponent.defaultProps = defaultProps;
-WebComponent.attributes = {
-  width: '100%',
-  height: '100%',
-};
+WebComponent.attributes = attributes;
 
 export default WebComponent;
