6dbf027d217a94cd9c57215604671d811a2843df
lucien <lucien.edel@geops.de>
2019-11-12 09:46:22 +0100

    Lint MapboxStyleLayer

--- src/layers/MapboxStyleLayer/MapboxStyleLayer.js
+++ src/layers/MapboxStyleLayer/MapboxStyleLayer.js
@@ -96,7 +96,7 @@ class MapboxStyleLayer extends Layer {
 
     // Apply the visibiltity when layer's visibility change.
     this.olListenersKeys.push(
-      this.on('change:visible', ({ target: layer }) => {        
+      this.on('change:visible', ({ target: layer }) => {
         if (mbMap.isStyleLoaded()) {
           applyVisibility(mbMap, layer.getVisible(), this.styleLayersFilter);
         }
