ed44f39f6483acbe665d1a5e5bbf0818ce87a3cb
lucien <lucien.edel@geops.de>
2019-11-29 11:10:32 +0100

    Use svg sbb logo instead.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import RSHeader from 'react-spatial/components/Header';
-import SBBLogo from '../../img/sbb-logo.png';
+import { ReactComponent as SBBLogo } from '../../img/sbb-logo.svg';
 
 import './Header.scss';
 
 const Header = () => (
   <RSHeader className="wkp-header">
     <div className="wkp-header-right">
-      <img src={SBBLogo} alt="SBB Logo" />
+      <SBBLogo focusable={false} />
     </div>
   </RSHeader>
 );
