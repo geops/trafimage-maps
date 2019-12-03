c80c6519caca0244b75a2a142117d27453395946
Eva Schiller | geOps <eva.schiller@geops.de>
2019-12-03 12:24:32 +0100

    Add Link component (with arrow)

---
+++ src/components/Link/Link.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withTranslation } from 'react-i18next';
+import { compose } from 'lodash/fp';
+import { ReactComponent as LinkIcon } from './Link.svg';
+
+import './Link.scss';
+
+const propTypes = {
+  href: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+const defaultProps = {
+  children: null,
+};
+
+const Link = ({ href, children }) => {
+  return (
+    <a
+      className="wkp-link"
+      href={href}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <LinkIcon />
+      {children}
+    </a>
+  );
+};
+
+Link.propTypes = propTypes;
+Link.defaultProps = defaultProps;
+
+export default compose(withTranslation())(Link);
--- src/popups/ConstructionPopup/ConstructionPopup.js
+++ src/popups/ConstructionPopup/ConstructionPopup.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Feature from 'ol/Feature';
 import { withTranslation } from 'react-i18next';
 import { compose } from 'lodash/fp';
+import Link from '../../components/Link';
 
 import './ConstructionPopup.scss';
 
