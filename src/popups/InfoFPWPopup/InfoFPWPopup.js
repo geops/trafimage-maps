b884d4ad7f26b67ad8097f0075b5939380856e62
Eva Schiller | geOps <eva.schiller@geops.de>
2019-12-05 11:08:48 +0100

    InfoFPW: Show images with react-slideshow-image lib

--- src/popups/InfoFPWPopup/InfoFPWPopup.js
+++ src/popups/InfoFPWPopup/InfoFPWPopup.js
@@ -4,8 +4,7 @@ import Feature from 'ol/Feature';
 import { withTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import { compose } from 'lodash/fp';
-import Button from 'react-spatial/components/Button';
-import { IoIosArrowRoundBack, IoIosArrowRoundForward } from 'react-icons/io';
+import { Fade } from 'react-slideshow-image';
 
 import './InfoFPWPopup.scss';
 
@@ -18,26 +17,11 @@ const propTypes = {
 const defaultProps = {};
 
 class InfoFPWPopup extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imageIndex: 0,
-    };
-  }
-
-  setImageIndex(index) {
-    this.setState({
-      imageIndex: index,
-    });
-  }
-
   render() {
     const { feature, language, t } = this.props;
-    const { imageIndex } = this.state;
 
     let text;
-    let image;
-    let pagination;
+    let images;
 
     const pureText = feature.get(`${language}_text`) || feature.get('de_text');
     if (pureText) {
@@ -47,43 +31,25 @@ class InfoFPWPopup extends PureComponent {
 
     const photoPath = feature.get('photo_path');
     if (photoPath) {
-      const photoPaths = [JSON.parse(photoPath), JSON.parse(photoPath)].flat();
+      const photoPaths = JSON.parse(photoPath);
 
       if (photoPaths.length) {
-        image = (
-          <img
-            src={`${process.env.REACT_APP_CARTARO_1_URL}/${photoPaths[imageIndex]}`}
-            draggable="false"
-            alt={t('Kein Bildtext')}
-          />
-        );
-      }
-
-      if (photoPaths.length > 1) {
-        pagination = (
-          <div className="wkp-pagination-wrapper">
-            <span className="wkp-pagination-button-wrapper">
-              {imageIndex > 0 ? (
-                <Button
-                  className="wkp-pagination-button"
-                  onClick={() => this.setImageIndex(imageIndex - 1)}
-                >
-                  <IoIosArrowRoundBack />
-                </Button>
-              ) : null}
-            </span>
-            {imageIndex + 1} {t('von')} {photoPaths.length}
-            <span className="wkp-pagination-button-wrapper">
-              {imageIndex + 1 < photoPaths.length ? (
-                <Button
-                  className="wkp-pagination-button"
-                  onClick={() => this.setImageIndex(imageIndex + 1)}
-                >
-                  <IoIosArrowRoundForward />
-                </Button>
-              ) : null}
-            </span>
-          </div>
+        images = (
+          <Fade
+            duration="500"
+            transitionDuration="500"
+            arrows={photoPaths.length > 1}
+            indicators={photoPaths.length > 1}
+            autoplay={false}
+          >
+            {photoPaths.map(url => (
+              <img
+                src={`${process.env.REACT_APP_CARTARO_1_URL}/${url}`}
+                draggable="false"
+                alt={t('Kein Bildtext')}
+              />
+            ))}
+          </Fade>
         );
       }
     }
@@ -91,10 +57,7 @@ class InfoFPWPopup extends PureComponent {
     return (
       <div className="wkp-infofpw-popup">
         {text}
-        <div className="wkp-infofpw-popup-images">
-          {image}
-          {pagination}
-        </div>
+        <div className="wkp-infofpw-popup-images">{images}</div>
       </div>
     );
   }
