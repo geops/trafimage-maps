8a1f4aa177478995fc54540196f363f16d750cab
lucien <lucien.edel@geops.de>
2019-12-06 12:13:35 +0100

    Start work on drawing button redirectin to the old wkp.

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import qs from 'query-string';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { TiImage, TiSocialFacebook, TiSocialTwitter } from 'react-icons/ti';
-import { FaEnvelope } from 'react-icons/fa';
+import { FaEnvelope, FaPencilAlt } from 'react-icons/fa';
+import { transform } from 'ol/proj';
 import CanvasSaveButton from 'react-spatial/components/CanvasSaveButton';
 import BlankLink from '@geops/react-ui/components/BlankLink';
+import Button from '@geops/react-ui/components/Button';
+import layerHelper from '../../layers/layerHelper';
 import SharePermalinkButton from '../SharePermalinkButton';
 
 const socialShareConfig = [
@@ -36,8 +40,34 @@ const renderConf = (conf, t) => (
   </div>
 );
 
+const redirectToDraw = topicKey => {
+  const urlParams = qs.parse(window.location.search);
+
+  if (urlParams.z) {
+    // Convert the zoom level to match the different scale on the old wkp.
+    urlParams.zoom = layerHelper.convertToOldZoom(parseInt(urlParams.z, 10));
+    delete urlParams.z;
+  }
+
+  if (urlParams.x || urlParams.y) {
+    // Reproject the coordinates to the old wkp projection: EPSG:21781.
+    const [newX, newY] = transform(
+      [parseInt(urlParams.x, 10), parseInt(urlParams.y, 10)],
+      'EPSG:3857',
+      'EPSG:21781',
+    );
+    urlParams.x = newX;
+    urlParams.y = newY;
+  }
+
+  window.location.href = `http://wkp.prod.trafimage.geops.ch/#/${topicKey}?${qs.stringify(
+    urlParams,
+  )}`;
+};
+
 const Share = () => {
   const map = useSelector(state => state.app.map);
+  const activeTopic = useSelector(state => state.app.activeTopic);
   const { t } = useTranslation();
   const config = [...socialShareConfig];
 
@@ -56,6 +86,14 @@ const Share = () => {
       </CanvasSaveButton>
       {renderConf(config[1], t)}
       {renderConf(config[2], t)}
+      <div className="ta-draw-icon">
+        <Button
+          onClick={() => redirectToDraw(activeTopic.key)}
+          title={`${t('Zeichnen')}.`}
+        >
+          <FaPencilAlt focusable={false} />
+        </Button>
+      </div>
     </div>
   );
 };
