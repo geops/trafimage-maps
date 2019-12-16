679e625e399f604c8600ccc25bc3863dfd608809
lucien <lucien.edel@geops.de>
2019-12-16 14:10:23 +0100

    Fix wrong translation and add missing spaces.

--- src/components/LegalLines/Contact/Contact.js
+++ src/components/LegalLines/Contact/Contact.js
@@ -32,21 +32,22 @@ const comps = {
   fr: (
     <div className="tm-imprint">
       <p>
-        Avete domande, proposte di miglioramento, segnalazioni di inesattezze o
-        siete interessati.
+        Vous avez des questions, des suggestions, des remarques sur des
+        inexactitudes ? Les cartes et plans vous intéressent? N’hésitez pas à
+        nos contacter.
       </p>
       <p>
-        <b>FFS SA</b>
+        <b>CFF SA</b>
         <br />
-        Infrastruttura – Impianti e tecnologia – Operation Center Tecnica –
-        Technical Management
+        Infrastructure – Installations et technologie – Operation Center
+        Technique – Technical Management
         <br />
         Fachbus Trafimage
         <br />
         Poststrasse 6<br />
         3072 Ostermundingen
         <br />
-        Svizzera
+        Suisse
       </p>
       <p>
         <a href="http://www.trafimage.ch">www.trafimage.ch</a>
