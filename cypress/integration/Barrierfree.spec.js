3916f28b757e93ea2910eb0341e4a4e28379afcb
lucien <lucien.edel@geops.de>
2020-01-13 12:11:56 +0100

    Add basic e2e test for barrierfree

---
+++ cypress/integration/Barrierfree.spec.js
@@ -0,0 +1,29 @@
+import 'cypress-plugin-tab';
+import lang from '../../src/lang/de.json';
+
+describe('Barrierfree E2E', () => {
+  beforeEach(() => {
+    cy.visit('');
+    cy.get('.wkp-menu-header ').click();
+  });
+
+  it('should show visible layer title in menu.', () => {
+    cy.get('.wkp-menu-header > .wkp-menu-title').contains(
+      lang['ch.sbb.netzkarte'],
+    );
+  });
+
+  it('should be able to navigate between menu and dialog with Tab.', () => {
+    // Move focus from menu to layer information button.
+    cy.tab().tab();
+    cy.focused().should('have.attr', 'class', 'wkp-info-topic-bt wkp-active');
+
+    // Press enter and focus dialog closer.
+    cy.focused().type('{enter}');
+    cy.focused().should('have.attr', 'class', 'tm-button tm-dialog-close-bt');
+
+    // Closer dialog and focus back on the information button.
+    cy.focused().type('{enter}');
+    cy.focused().should('have.attr', 'class', 'wkp-info-topic-bt wkp-active');
+  });
+});
