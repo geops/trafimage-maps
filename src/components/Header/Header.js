bf811f15eb1bcf00d11ba650387eec5bbc37c2ac
Mario Härtwig <mario.haertwig@geops.de>
2020-01-20 08:50:55 +0100

    Login button.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { AiOutlineUser } from 'react-icons/ai';
 import UIHeader from '@geops/react-ui/components/Header';
 import { ReactComponent as SBBLogo } from '../../img/sbb-logo.svg';
 
 import './Header.scss';
 
-const Header = () => (
-  <UIHeader className="wkp-header">
-    <div className="wkp-header-right">
-      <a href={process.env.REACT_APP_LOGIN_URL}>Login</a>
-      <SBBLogo focusable={false} />
-    </div>
-  </UIHeader>
-);
+const Header = () => {
+  let login = <a href={`${process.env.REACT_APP_OLD_WKP}/login`}>Login</a>;
+  const permissions = useSelector(state => state.app.permissions);
+
+  if (permissions.user) {
+    login = permissions.user;
+  }
+
+  return (
+    <UIHeader className="wkp-header">
+      <div className="wkp-header-right">
+        <div className="wkp-header-login">
+          <AiOutlineUser className="wkp-header-login-icon" />
+          <span className="wkp-header-login-text">{login}</span>
+        </div>
+        <SBBLogo focusable={false} />
+      </div>
+    </UIHeader>
+  );
+};
 
 export default Header;
