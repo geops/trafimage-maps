b1aff391b41d3ea08573a61ac90a4b1c85fb5e4b
gevos <eva.schiller@geops.de>
2020-02-17 16:42:33 +0100

    Mouse position: Use default projection from redux in mouse position (#480)

---
+++ src/utils/coordinateHelper.js
@@ -0,0 +1,13 @@
+function meterFormat(coords) {
+  return coords.map(num =>
+    Math.round(num)
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, "'"),
+  );
+}
+
+function wgs84Format(coords, decimalSep = '.') {
+  return coords.map(num => num.toFixed(5).replace('.', decimalSep));
+}
+
+export default { meterFormat, wgs84Format };
