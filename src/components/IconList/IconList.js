98e67dad2bddd4c9abebfd639371928e54f70798
Eva Schiller | geOps <eva.schiller@geops.de>
2020-03-31 12:57:33 +0200

    Core: Zweitausbildung: Add displayOnTop option to IconList...
    This options appends the expanded list (dropdown)
    to the element with the class tm-trafimage-maps
    (body would be not in our scope).
    
    Then the list is displayed in modal mode
    in order to avoid scrolling which would break the position.
    
    The normal inline option (displayOnTop=false) is still available.

--- src/components/IconList/IconList.js
+++ src/components/IconList/IconList.js
@@ -1,4 +1,5 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, createRef } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import List from '@geops/react-ui/components/List';
 import Button from '@geops/react-ui/components/Button';
@@ -23,6 +24,7 @@ const propTypes = {
   }),
 
   onSelect: PropTypes.func.isRequired,
+  displayOnTop: PropTypes.bool,
   t: PropTypes.func.isRequired,
 };
 
@@ -33,11 +35,14 @@ const defaultProps = {
     height: 16,
     width: 42,
   },
+  displayOnTop: false,
 };
 
 class IconList extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.ref = createRef();
 
     this.state = {
       selectedOption: props.selected,
@@ -64,11 +69,11 @@ class IconList extends PureComponent {
 
     const opt = option || (options.length ? options[0] : null);
 
-    const label = <div className="wkp-image-dropdown-label">{t(opt)}</div>;
+    const label = <div className="wkp-icon-list-label">{t(opt)}</div>;
 
     return opt && icons[opt] ? (
       <>
-        <div className="wkp-image-dropdown-icon">
+        <div className="wkp-icon-list-icon">
           <img
             src={icons[option]}
             height={iconSize.height}
@@ -84,14 +89,61 @@ class IconList extends PureComponent {
     );
   }
 
+  renderList() {
+    const { displayOnTop, options, onSelect } = this.props;
+
+    const list = (
+      <List
+        items={options}
+        className="tm-list wkp-icon-list-list"
+        renderItem={option => this.renderOption(option)}
+        getItemKey={option => option}
+        onSelect={(e, option) => {
+          onSelect(option);
+          this.setState({
+            iconListVis: false,
+            selectedOption: option,
+          });
+        }}
+      />
+    );
+
+    if (displayOnTop) {
+      const rect = this.ref.current.getBoundingClientRect();
+      const listStyles = {
+        top: `${rect.y + rect.height}px`,
+        left: `${rect.x}px`,
+      };
+
+      const targetElements = document.getElementsByClassName(
+        'tm-trafimage-maps',
+      );
+
+      if (!targetElements || !targetElements.length) {
+        return null;
+      }
+
+      return ReactDOM.createPortal(
+        <div className="wkp-icon-list-modal">
+          <div className="wkp-icon-list-modal-pos" style={listStyles}>
+            {list}
+          </div>
+        </div>,
+        targetElements[0],
+      );
+    }
+
+    return <div className="wkp-icon-list-non-modal">{list}</div>;
+  }
+
   render() {
-    const { disabled, options, onSelect } = this.props;
+    const { disabled } = this.props;
     const { iconListVis, selectedOption } = this.state;
 
     return (
-      <div>
+      <div ref={this.ref}>
         <Button
-          className="wkp-image-dropdown-button"
+          className="wkp-icon-list-button"
           onClick={() => {
             if (disabled) {
               return;
@@ -99,7 +151,7 @@ class IconList extends PureComponent {
             this.setState({ iconListVis: !iconListVis });
           }}
         >
-          <div className="wkp-image-dropdown-current">
+          <div className="wkp-icon-list-current">
             {this.renderOption(selectedOption)}
           </div>
           <div className="pe-caret">
@@ -110,21 +162,7 @@ class IconList extends PureComponent {
             )}
           </div>
         </Button>
-        {iconListVis && !disabled ? (
-          <List
-            items={options}
-            className="tm-list wkp-image-dropdown-list"
-            renderItem={option => this.renderOption(option)}
-            getItemKey={option => option}
-            onSelect={(e, option) => {
-              onSelect(option);
-              this.setState({
-                iconListVis: false,
-                selectedOption: option,
-              });
-            }}
-          />
-        ) : null}
+        {iconListVis && !disabled ? this.renderList() : null}
       </div>
     );
   }
