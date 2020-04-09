d9d112e9ef73736eed44d5674f7eaadcf4701f46
Eva Schiller | geOps <eva.schiller@geops.de>
2020-04-09 08:20:07 +0200

    Zweitausbildung: Use the unique label to filter, sort, and compare features...
    ...This fixes the bug
    where tourist routes with the same bezeichnung were missing in the popup

--- src/layers/ZweitausbildungRoutesHighlightLayer/ZweitausbildungRoutesHighlightLayer.js
+++ src/layers/ZweitausbildungRoutesHighlightLayer/ZweitausbildungRoutesHighlightLayer.js
@@ -19,6 +19,15 @@ import IconList from '../../components/IconList';
  * @param {Object} [options] Layer options.
  */
 class ZweitausbildungRoutesHighlightLayer extends VectorLayer {
+  static generateLabel(feature) {
+    // viadescription for tourist routes
+    // description for hauptlinien
+    const description =
+      feature.get('viadescription') || feature.get('description');
+    const desc = description ? `: ${description}` : '';
+    return `${feature.get('bezeichnung')}${desc}`;
+  }
+
   constructor(options = {}) {
     const olLayer = new OLVectorLayer({
       style: (f, r) => this.style(f, r),
@@ -93,14 +102,9 @@ class ZweitausbildungRoutesHighlightLayer extends VectorLayer {
     for (let i = 0; i < this.features.length; i += 1) {
       const feature = this.features[i];
 
-      // viadescription for tourist routes
-      // description for hauptlinien
-      const description =
-        feature.get('viadescription') || feature.get('description');
-      const desc = description ? `: ${description}` : '';
-      const label = `${feature.get('bezeichnung')}${desc}`;
-
+      const label = ZweitausbildungRoutesHighlightLayer.generateLabel(feature);
       this.features[i].set('label', label);
+
       if (this.options.indexOf(label) === -1) {
         this.options.push(label);
         this.icons[label] = feature.get('line_number')
@@ -177,6 +181,14 @@ class ZweitausbildungRoutesHighlightLayer extends VectorLayer {
       .then((data) => {
         const format = new GeoJSON();
         const feats = format.readFeatures(data);
+
+        // Set the unique label
+        for (let i = 0; i < feats.length; i += 1) {
+          const label = ZweitausbildungRoutesHighlightLayer.generateLabel(
+            feats[i],
+          );
+          feats[i].set('label', label);
+        }
 
         const features = [];
         if (feats.length) {
