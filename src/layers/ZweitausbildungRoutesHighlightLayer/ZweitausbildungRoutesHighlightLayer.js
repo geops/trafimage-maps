4bdf772ef8036ce7d0584a74577b92c27cb68f4b
oterral <oterral@users.noreply.github.com>
2020-04-09 13:35:47 +0200

    Zweitausbildung: Remove use of create portal (#654)

--- src/layers/ZweitausbildungRoutesHighlightLayer/ZweitausbildungRoutesHighlightLayer.js
+++ src/layers/ZweitausbildungRoutesHighlightLayer/ZweitausbildungRoutesHighlightLayer.js
@@ -138,7 +138,6 @@ class ZweitausbildungRoutesHighlightLayer extends VectorLayer {
           selected={this.selected}
           icons={this.icons}
           onSelect={this.onSelect}
-          displayOnTop
         />
       );
     }
