234e25d2f8e430b14e4ce6d0362fbe5c1416753a
Eva Schiller | geOps <eva.schiller@geops.de>
2020-04-21 08:52:55 +0200

    Replace remaining resolutions by LayerHelper.mapResolutions

--- src/config/layers.js
+++ src/config/layers.js
@@ -40,29 +40,6 @@ const projectionExtent = [
   20037508.3428,
 ];
 
-const resolutions = [
-  156543.033928,
-  78271.516964,
-  39135.758482,
-  19567.879241,
-  9783.9396205,
-  4891.96981025,
-  2445.98490513,
-  1222.99245256,
-  611.496226281,
-  305.748113141,
-  152.87405657,
-  76.4370282852,
-  38.2185141426,
-  19.1092570713,
-  9.55462853565,
-  4.77731426782,
-  2.38865713391,
-  1.19432856696,
-  0.597164283478,
-  0.298582141739,
-];
-
 export const dataLayer = new TrafimageMapboxLayer({
   name: 'ch.sbb.netzkarte.data',
   visible: true,
@@ -888,8 +865,8 @@ export const zweitausbildungStations = new Layer({
           },
           tileGrid: new TileGrid({
             extent: projectionExtent,
-            resolutions,
-            matrixIds: resolutions.map((r, i) => `${i}`),
+            resolutions: LayerHelper.mapResolutions,
+            matrixIds: LayerHelper.mapResolutions.map((r, i) => `${i}`),
           }),
         }),
       }),
@@ -928,8 +905,8 @@ export const zweitausbildungStations = new Layer({
           },
           tileGrid: new TileGrid({
             extent: projectionExtent,
-            resolutions,
-            matrixIds: resolutions.map((r, i) => `${i}`),
+            resolutions: LayerHelper.mapResolutions,
+            matrixIds: LayerHelper.mapResolutions.map((r, i) => `${i}`),
           }),
         }),
       }),
