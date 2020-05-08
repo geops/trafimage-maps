749827aac0ed79b26708d34aa7f506cc2987aa9c
edellucien <edellucien@gmail.com>
2020-05-08 12:05:24 +0200

    Fix baselayer style filter for netzkarte v3

--- src/config/layers.js
+++ src/config/layers.js
@@ -138,7 +138,7 @@ export const swisstopoLandeskarte = new MapboxStyleLayer({
   visible: false,
   mapboxLayer: dataLayer,
   styleLayersFilter: (styleLayer) => {
-    return /pixelkarte_farbe/.test(styleLayer.id);
+    return /img_PK_farbe/.test(styleLayer.id);
   },
 });
 
@@ -150,7 +150,7 @@ export const swisstopoLandeskarteGrau = new MapboxStyleLayer({
   visible: false,
   mapboxLayer: dataLayer,
   styleLayersFilter: (styleLayer) => {
-    return /pixelkarte_grau/.test(styleLayer.id);
+    return /img_PK_grau/.test(styleLayer.id);
   },
 });
 
