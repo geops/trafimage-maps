f2adbb2f8195b4384f1e873b87c96013b0e5cce6
edellucien <edellucien@gmail.com>
2020-08-12 09:31:28 +0200

    Use immobilien vector tiles

--- src/menus/ImmobilienMenuBottom/ImmobilienMenuBottom.js
+++ src/menus/ImmobilienMenuBottom/ImmobilienMenuBottom.js
@@ -9,47 +9,56 @@ const propTypes = {
 
 const filters = {
   Alle: undefined,
-  'ch.sbb.stuetzpunktbahnhoefe': ['all', ['==', 'stuetzpunktbahnhof', true]],
-  'ch.sbb.barrierfreierbahnhoefe': [
-    'all',
-    ['==', 'barrierefreier_bahnhof', true],
-  ],
-  'ch.sbb.nichtbarrierfreierbahnhoefe': [
-    'all',
-    ['==', 'barrierefreier_bahnhof', false],
-  ],
+  ROT: ['all', ['==', 'region', 'ROT']],
+  RME: ['all', ['==', 'region', 'RME']],
+  RWT: ['all', ['==', 'region', 'RWT']],
 };
 
 const ImmobilienMenuBottom = ({ topic }) => {
+  const onChangeCallback = (value) => {
+    topic.layers
+      .filter((l) => !l.isBaseLayer && l instanceof MapboxStyleLayer)
+      .forEach((layer) => {
+        if (layer) {
+          const { mbMap } = layer.mapboxLayer;
+          const style = mbMap.getStyle();
+          if (!mbMap || !style) {
+            return;
+          }
+
+          for (let i = 0; i < style.layers.length; i += 1) {
+            const styleLayer = style.layers[i];
+            mbMap.setFilter(styleLayer.id, filters[value]);
+          }
+        }
+      });
+  };
+
   return (
     <div className="wkp-topic-filter">
       <SelectFilter
-        label="Kategorie"
+        label="Region"
         choices={{
           a0: 'Alle',
-          a1: 'ch.sbb.stuetzpunktbahnhoefe',
-          a2: 'ch.sbb.barrierfreierbahnhoefe',
-          a3: 'ch.sbb.nichtbarrierfreierbahnhoefe',
+          a1: 'ROT',
+          a2: 'RME',
+          a3: 'RWT',
         }}
         defaultValue="a0"
-        onChangeCallback={(value) => {
-          topic.layers
-            .filter((l) => !l.isBaseLayer && l instanceof MapboxStyleLayer)
-            .forEach((layer) => {
-              if (layer) {
-                const { mbMap } = layer.mapboxLayer;
-                const style = mbMap.getStyle();
-                if (!mbMap || !style) {
-                  return;
-                }
-
-                for (let i = 0; i < style.layers.length; i += 1) {
-                  const styleLayer = style.layers[i];
-                  mbMap.setFilter(styleLayer.id, filters[value]);
-                }
-              }
+        onChangeCallback={onChangeCallback}
+      />
+      <SelectFilter
+        label="Leistungtypen"
+        fetchChoices={() => {
+          return fetch(
+            `https://cartaro2.dev.trafimage.ch/api/v1/immobilien/leistungstypen`,
+          )
+            .then((res) => res.json())
+            .then((res) => {
+              return res;
             });
         }}
+        onChangeCallback={onChangeCallback}
       />
     </div>
   );
