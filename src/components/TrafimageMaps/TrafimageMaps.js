1b5ab1c358932ff1bbfc9f5df9b604da475cb093
edellucien <edellucien@gmail.com>
2020-08-20 14:50:32 +0200

    Remove unnecessary

--- src/components/TrafimageMaps/TrafimageMaps.js
+++ src/components/TrafimageMaps/TrafimageMaps.js
@@ -9,7 +9,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { Layer } from 'mobility-toolbox-js/ol';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import TopicLoader from '../TopicLoader';
 import { getStore } from '../../model/store';
 import { setZoom, setCenter, setMaxExtent } from '../../model/map/actions';
@@ -161,28 +160,6 @@ class TrafimageMaps extends React.PureComponent {
      * @private
      */
     this.store = getStore();
-
-    this.theme = createMuiTheme({
-      palette: {
-        primary: {
-          main: '#000',
-        },
-        text: {
-          primary: '#000',
-        },
-      },
-      typography: {
-        fontFamily: ['SBBWeb-Roman', 'Arial', 'sans-serif'],
-        fontSize: 12,
-      },
-      overrides: {
-        MuiIconButton: {
-          root: {
-            fontSize: '1.5rem',
-          },
-        },
-      },
-    });
   }
 
   componentDidMount() {
@@ -268,20 +245,18 @@ class TrafimageMaps extends React.PureComponent {
     return (
       <MatomoProvider value={enableTracking && matomo}>
         <Provider store={this.store}>
-          <ThemeProvider theme={this.theme}>
-            <TopicLoader
-              history={history}
-              apiKey={apiKey}
-              topics={topics}
-              activeTopicKey={activeTopicKey}
-              cartaroUrl={cartaroUrl}
-              appBaseUrl={appBaseUrl}
-              permissionUrl={permissionUrl}
-              vectorTilesKey={vectorTilesKey}
-              vectorTilesUrl={vectorTilesUrl}
-              staticFilesUrl={staticFilesUrl}
-            />
-          </ThemeProvider>
+          <TopicLoader
+            history={history}
+            apiKey={apiKey}
+            topics={topics}
+            activeTopicKey={activeTopicKey}
+            cartaroUrl={cartaroUrl}
+            appBaseUrl={appBaseUrl}
+            permissionUrl={permissionUrl}
+            vectorTilesKey={vectorTilesKey}
+            vectorTilesUrl={vectorTilesUrl}
+            staticFilesUrl={staticFilesUrl}
+          />
         </Provider>
       </MatomoProvider>
     );
