979e1e251189be85741090fbd4e3a6529c7621a6
Olivier Terral <olivier.terral@geops.de>
2020-10-21 16:02:00 +0200

    Add MUI theme

--- src/filters/AusbauFilters/AusbauFilters.js
+++ src/filters/AusbauFilters/AusbauFilters.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import qs from 'query-string';
 import Layer from 'mobility-toolbox-js/common/layers/Layer';
-import SelectFilter from '../SelectFilter';
+import {
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  formControl: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+}));
 
 const propTypes = {
   layer: PropTypes.instanceOf(Layer),
@@ -17,7 +30,7 @@ const FILTER_PROP = 'angebotsschritt';
 const DATA_LAYER_PROP = 'dataLayer';
 const initialFiltersById = {};
 
-const onChange = (values, layers) => {
+const onChange = (value, layers) => {
   layers.forEach(({ mbMap }) => {
     const style = mbMap.getStyle();
     if (!mbMap || !style) {
@@ -27,6 +40,11 @@ const onChange = (values, layers) => {
     style.layers.forEach(({ id }) => {
       // console.log(id, mbMap.getFilter(id));
 
+      // This filter must affect nur Ausbau layers.
+      if (!/ausbau/.test(id)) {
+        return;
+      }
+
       // Store the initial filter values for each style layer.
       if (!initialFiltersById[id]) {
         initialFiltersById[id] = mbMap.getFilter(id) || [];
@@ -35,61 +53,53 @@ const onChange = (values, layers) => {
       let newFilter = initialFiltersById[id].length
         ? [...initialFiltersById[id]]
         : null;
-      // console.log('init filter:', newFilter);
 
-      // We create an 'any' filter with all the choices selected.
-      const filterToApply = ['any'];
+      const filterToApply = ['==', value, ['get', FILTER_PROP]];
 
-      values.forEach((value) => {
-        if (value !== undefined) {
-          const filter = ['==', '', ['get', FILTER_PROP]];
-          filterToApply.push(filter);
-        }
-      });
-
-      // If some choices are selected we apply the 'any' filter
-      // or we push it to the inital 'all' style layer filter.
-      if (values.length) {
+      if (value !== undefined) {
         if (!newFilter) {
-          newFilter = filterToApply;
+          newFilter = ['all', filterToApply];
         } else if (newFilter[0] === 'all') {
           // we assume the filter is define with 'all' keyword
           // ['all', filter1, filter2]
           newFilter.push(filterToApply);
         }
       }
-      // console.log(newFilter);
       mbMap.setFilter(id, newFilter);
     });
   });
 };
 
 const AusbauFilters = ({ layer }) => {
+  const classes = useStyles();
   const { t } = useTranslation();
+  const [value, setValue] = useState('');
   const isShowFilter = qs.parse(window.location.search).showFilter === 'true';
+
+  const updateFilter = useCallback(
+    (evt) => {
+      const newValue = evt.target.value;
+      setValue(newValue);
+      onChange(newValue, [layer.get(DATA_LAYER_PROP)]);
+    },
+    [layer, setValue],
+  );
 
   if (!isShowFilter || !layer || !layer.get(DATA_LAYER_PROP)) {
     return null;
   }
-  const dataLayer = layer.get(DATA_LAYER_PROP);
 
   return (
-    <SelectFilter
-      multiple
-      label={t(FILTER_PROP)}
-      choices={[
-        { value: '', label: 'Projekte im Bau' },
-        {
-          value: '2030',
-          label: 'Fertigstellung bis 2030 ',
-        },
-        { value: '2035', label: 'Fertigstellung bis 2035 ' },
-      ]}
-      onChange={(values) => {
-        // console.log(values);
-        onChange(values, [dataLayer]);
-      }}
-    />
+    <>
+      <FormControl fullWidth className={classes.formControl}>
+        <InputLabel shrink>{t(FILTER_PROP)}</InputLabel>
+        <Select autoWidth displayEmpty value={value} onChange={updateFilter}>
+          <MenuItem value="">{t('Projekte im Bau')}</MenuItem>
+          <MenuItem value="2030">{t('Fertigstellung bis 2030')}</MenuItem>
+          <MenuItem value="2035">{t('Fertigstellung bis 2035')}</MenuItem>
+        </Select>
+      </FormControl>
+    </>
   );
 };
 
