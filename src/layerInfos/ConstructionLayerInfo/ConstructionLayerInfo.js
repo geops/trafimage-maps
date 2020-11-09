ce1b85c7370366085b8cc691e73764414de3e585
edellucien <edellucien@gmail.com>
2020-11-09 16:28:48 +0100

    Use orange icons for constructions

--- src/layerInfos/ConstructionLayerInfo/ConstructionLayerInfo.js
+++ src/layerInfos/ConstructionLayerInfo/ConstructionLayerInfo.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
-
+import constructionIcons from './images';
 import './ConstructionLayerInfo.scss';
 
 const propTypes = {
   t: PropTypes.func.isRequired,
   properties: PropTypes.object.isRequired,
-  staticFilesUrl: PropTypes.string.isRequired,
 };
 
 const defaultProps = {};
 
-const ConstructionLayerInfo = ({ t, properties, staticFilesUrl }) => {
+const ConstructionLayerInfo = ({ t, properties }) => {
   const config = properties.get('construction');
   const filename = `${config.art}_${config.ort}`.replace(
     /[^A-Z,^0-9,-_]/gi,
@@ -22,7 +21,7 @@ const ConstructionLayerInfo = ({ t, properties, staticFilesUrl }) => {
   return (
     <div className="wkp-construction-layer-info">
       <img
-        src={`${staticFilesUrl}/img/layers/construction/${filename}.png`}
+        src={constructionIcons[filename]}
         draggable="false"
         alt={t('Kein Bildtext')}
       />
---
+++ src/layerInfos/ConstructionLayerInfo/images/index.js
@@ -0,0 +1,22 @@
+import AusbauBahnhof from './Ausbau_Bahnhof.svg';
+import AusbauBahnhofundStrecke from './Ausbau_BahnhofundStrecke.svg';
+import AusbauBrigeStandorte from './Ausbau_brigeStandorte.svg';
+import AusbauStrecke from './Ausbau_Strecke.svg';
+
+import UnterhaltBahnhof from './Unterhalt_Bahnhof.svg';
+import UnterhaltBahnhofundStrecke from './Unterhalt_BahnhofundStrecke.svg';
+import UnterhaltBrigeStandorte from './Unterhalt_brigeStandorte.svg';
+import UnterhaltStrecke from './Unterhalt_Strecke.svg';
+
+const constructionIcons = {
+  Ausbau_Bahnhof: AusbauBahnhof,
+  Ausbau_BahnhofundStrecke: AusbauBahnhofundStrecke,
+  Ausbau_brigeStandorte: AusbauBrigeStandorte,
+  Ausbau_Strecke: AusbauStrecke,
+  Unterhalt_Bahnhof: UnterhaltBahnhof,
+  Unterhalt_BahnhofundStrecke: UnterhaltBahnhofundStrecke,
+  Unterhalt_brigeStandorte: UnterhaltBrigeStandorte,
+  Unterhalt_Strecke: UnterhaltStrecke,
+};
+
+export default constructionIcons;
