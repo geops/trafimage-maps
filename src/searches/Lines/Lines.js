cd0634b2a8fb41c4ab7bb64cd421cb84803139a2
edellucien <edellucien@gmail.com>
2020-12-15 12:12:21 +0100

    Adapt line generalisations

--- src/searches/Lines/Lines.js
+++ src/searches/Lines/Lines.js
@@ -14,14 +14,13 @@ const lineKilometerRegExp = new RegExp('^([0-9]+)\\s+([0-9]+\\.?[0-9]+)$');
 
 const color = 'rgba(0,61,155,0.5)';
 
-const lineResolutions = [900, 850, 500, 250, 115, 100, 75, 50, 20, 10, 5];
+const lineResolutions = [900, 611, 305, 115, 100, 75, 50, 20, 10, 5];
 const lineGeneralisations = {
   900: 5,
-  850: 10,
-  500: 10,
-  250: 30,
+  611: 10,
+  305: 30,
   115: 30,
-  100: 100,
+  100: 30,
   75: 100,
   50: 100,
 };
