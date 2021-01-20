ae6a20cd4a1bd7ad3f23e040bd50f0cdad2af468
edellucien <edellucien@gmail.com>
2021-01-20 14:08:36 +0100

    Use new mobility-toolbox v 1.2.7 - New punctuality delay colors. Adapt legend accordingly

--- src/layerInfos/PunctualityLayerInfo/PunctualityLayerInfo.js
+++ src/layerInfos/PunctualityLayerInfo/PunctualityLayerInfo.js
@@ -2,28 +2,74 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 
+import './PunctualityLayerInfo.scss';
+
 const propTypes = {
   t: PropTypes.func.isRequired,
-  staticFilesUrl: PropTypes.string.isRequired,
 };
 
 const defaultProps = {};
 
-const PunctualityLayerInfo = ({ language, t, staticFilesUrl }) => {
-  const img = (
-    <img
-      src={`${staticFilesUrl}/img/layers/puenktlichkeit/puenktlichkeit_legend_${language}.png`}
-      draggable="false"
-      alt={t('Kein Bildtext')}
-    />
-  );
+const PunctualityLayerInfo = ({ language, t }) => {
+  const delays = [
+    {
+      color: '#00a00c', // green
+      legend: 'ch.sbb.puenktlichkeit-ontime',
+    },
+    {
+      color: '#f7bf00', // yellow
+      legend: 'ch.sbb.puenktlichkeit-medium-delay',
+    },
+    {
+      color: '#e13f20', // red
+      legend: 'ch.sbb.puenktlichkeit-high-delay',
+    },
+    {
+      color: '#a0a0a0', // grey
+      legend: 'ch.sbb.puenktlichkeit-company-no-realtime',
+    },
+    {
+      crossed: true,
+      color: '#a0a0a0', // grey
+      legend: 'ch.sbb.puenktlichkeit-journey-no-realtime',
+    },
+  ];
+
+  const legend = delays.map((d) => (
+    <div className="tm-punctuality-delay">
+      <svg height="21" width="21">
+        <circle cx="10" cy="10" r="10" fill={d.color} />
+        {d.crossed ? (
+          <>
+            <line
+              x1="3"
+              y1="17"
+              x2="17"
+              y2="3"
+              strokeWidth="1.1"
+              stroke="black"
+            />
+            <line
+              x1="17"
+              y1="17"
+              x2="3"
+              y2="3"
+              strokeWidth="1.1"
+              stroke="black"
+            />
+          </>
+        ) : null}
+      </svg>
+      {t(d.legend)}
+    </div>
+  ));
 
   const comps = {
     de: (
       <div>
         Der Zugtracker zeigt die aktuellen Verbindungen basierend auf dem Soll-
         und Ist-Fahrplan des Schweizer ÖV.
-        <p>{img}</p>
+        <p>{legend}</p>
         <p>
           Daten:&nbsp;
           <a
@@ -41,7 +87,7 @@ const PunctualityLayerInfo = ({ language, t, staticFilesUrl }) => {
       <div>
         Le train tracker indique les trajets en cours en se basant sur l’horaire
         théorique et réel des transports publiques suisses.
-        <p>{img}</p>
+        <p>{legend}</p>
         <p>
           Données:&nbsp;
           <a
@@ -59,7 +105,7 @@ const PunctualityLayerInfo = ({ language, t, staticFilesUrl }) => {
       <div>
         The train tracker shows current connections based on the planned and
         actual timetable for Swiss public transport.
-        <p>{img}</p>
+        <p>{legend}</p>
         <p>
           Data:&nbsp;
           <a
@@ -77,7 +123,7 @@ const PunctualityLayerInfo = ({ language, t, staticFilesUrl }) => {
       <div>
         Il train tracker mostra i collegamenti aggiornati sulla base dell’orario
         teorico e attuale dei trasporti pubblici svizzeri.
-        <p>{img}</p>
+        <p>{legend}</p>
         <p>
           Dati:&nbsp;
           <a
