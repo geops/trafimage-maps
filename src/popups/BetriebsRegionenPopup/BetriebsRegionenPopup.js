13a91107e60fc270b244f8b17bdc924dbff785e1
edellucien <edellucien@gmail.com>
2021-03-31 13:51:53 +0200

    [TRAFDIV-432] Add click function to the regionen vector layer

--- src/popups/BetriebsRegionenPopup/BetriebsRegionenPopup.js
+++ src/popups/BetriebsRegionenPopup/BetriebsRegionenPopup.js
@@ -1,15 +1,59 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
+import Feature from 'ol/Feature';
 
 const propTypes = {
-  properties: PropTypes.object.isRequired,
+  language: PropTypes.string.isRequired,
+  feature: PropTypes.instanceOf(Feature).isRequired,
 };
 
-const BetriebsRegionenPopup = ({ properties }) => {
-  const { region } = properties;
+const regions = {
+  'Region West': {
+    de: 'Region West',
+    en: 'West Region',
+    it: 'Regione Ovest',
+    fr: 'Région Ouest',
+  },
+  'Region Mitte': {
+    de: 'Region Mitte',
+    en: 'Center Region',
+    it: 'Regione Centro',
+    fr: 'Région Centre',
+  },
+  'Region Süd': {
+    de: 'Region Süd',
+    en: 'South Region',
+    it: 'Regione Sud',
+    fr: 'Région Sud',
+  },
+  'Region Ost': {
+    de: 'Region Ost',
+    en: 'East Region',
+    it: 'Regione Est',
+    fr: 'Région Est',
+  },
+  'Andere ISB BLS': {
+    de: 'Andere ISB BLS',
+    en: 'Other ISB BLS',
+    it: 'Altro ISB BLS',
+    fr: 'Autre ISB BLS',
+  },
+  'Andere ISB SOB': {
+    de: 'Andere ISB SOB',
+    en: 'Other ISB SOB',
+    it: 'Altro Est',
+    fr: 'Autre ISB SOB',
+  },
+};
+
+const BetriebsRegionenPopup = ({ language, feature }) => {
+  const regionName = feature.get('region');
+  if (!regions[regionName]) {
+    return null;
+  }
   return (
     <div>
-      <span>{region}</span>
+      <span>{regions[regionName][language]}</span>
     </div>
   );
 };
