fc96555245ae3d1a6cd82f1b2f3bab30c49fc555
Olivier Terral <olivier.terral@geops.de>
2021-05-06 17:12:25 +0200

    Add intersection point popup

---
+++ src/popups/RegionenkarteIntersectionPopup/RegionenkarteIntersectionPopup.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Feature from 'ol/Feature';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: '100px !important',
+  },
+}));
+
+function RegionenkarteIntersectionPopup({ feature }) {
+  const classes = useStyles();
+  return <div className={classes.root}>{feature.get('label')}</div>;
+}
+
+RegionenkarteIntersectionPopup.propTypes = {
+  feature: PropTypes.instanceOf(Feature).isRequired,
+};
+
+RegionenkarteIntersectionPopup.hideHeader = () => true;
+
+export default RegionenkarteIntersectionPopup;
