6c65b89e63a8869b6fb982d6212c927a6e69222b
Olivier Terral <olivier.terral@geops.de>
2021-06-18 15:15:08 +0200

    Don't remove viewbox

--- styleguide.config.js
+++ styleguide.config.js
@@ -110,6 +110,11 @@ module.exports = {
               loader: require.resolve('file-loader'),
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
+                svgoConfig: {
+                  plugins: {
+                    removeViewBox: false,
+                  },
+                },
               },
             },
           ],
