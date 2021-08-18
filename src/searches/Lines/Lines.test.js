9bb0a6adfa743c213b7be9cb5c529b5972939b7b
danji90 <daniel.marsh-hunn@geops.de>
2021-08-18 22:16:56 +0200

    added more tests

--- src/searches/Lines/Lines.test.js
+++ src/searches/Lines/Lines.test.js
@@ -34,11 +34,29 @@ describe('Lines', () => {
     });
   });
 
+  describe('should not insert the km parameter in a km search request', () => {
+    test('with no subsequent whitespace after line number (500+50)', async () => {
+      await linesSearch.search('500+50');
+      expect(fetchMock).toHaveBeenCalledWith(
+        'https://maps.trafimage.ch/search/lines?line=500%2B50',
+      );
+    });
+  });
+
   describe('should insert the km and km_end parameters correctly in a line segment search request', () => {
     test('with 500 10-50', async () => {
       await linesSearch.search('500 10-50');
       expect(fetchMock).toHaveBeenCalledWith(
         'https://maps.trafimage.ch/search/lines?line=500&km=10&km_end=50',
+      );
+    });
+  });
+
+  describe('should not insert the km and km_end parameters correctly in a line segment search request', () => {
+    test('with a wrong pattern (500 50 -80)', async () => {
+      await linesSearch.search('500 50 -80');
+      expect(fetchMock).toHaveBeenCalledWith(
+        'https://maps.trafimage.ch/search/lines?line=500%2050%20-80',
       );
     });
   });
