a62fc7042d660c3c2b5ba6bbb52774e4e7363730
Olivier Terral <olivier.terral@geops.de>
2021-09-08 12:09:36 +0200

    Add translations

--- src/popups/DirektverbindungPopup/DirektverbindungPopup.js
+++ src/popups/DirektverbindungPopup/DirektverbindungPopup.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Feature from 'ol/Feature';
-import MapboxStyleLayer from '../../layers/MapboxStyleLayer';
 
 const useStyles = makeStyles({
   container: {
