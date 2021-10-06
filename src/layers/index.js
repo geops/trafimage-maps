f9e7d9ea6569b36a36c554ca689cfb73ec5161a9
Olivier Terral <olivier.terral@geops.de>
2021-10-06 17:37:04 +0200

    Manage all kind of exports

--- src/layers/index.js
+++ src/layers/index.js
@@ -15,6 +15,23 @@ import ZweitausbildungPoisLayer from './ZweitausbildungPoisLayer';
 import ZweitausbildungRoutesHighlightLayer from './ZweitausbildungRoutesHighlightLayer';
 import ZweitausbildungRoutesLayer from './ZweitausbildungRoutesLayer';
 
+export { default as AusbauLayer } from './AusbauLayer';
+export { default as CasaLayer } from './CasaLayer';
+export { default as DrawLayer } from './DrawLayer';
+export { default as KilometrageLayer } from './KilometrageLayer';
+export { default as LevelLayer } from './LevelLayer';
+export { default as MapboxStyleLayer } from './MapboxStyleLayer';
+export { default as RouteLayer } from './RouteLayer';
+export { default as StationsLayer } from './StationsLayer';
+export { default as TarifverbundkarteLayer } from './TarifverbundkarteLayer';
+export { default as TrafimageGeoServerWMSLayer } from './TrafimageGeoServerWMSLayer';
+export { default as TrafimageMapboxLayer } from './TrafimageMapboxLayer';
+export { default as ZoneLayer } from './ZoneLayer';
+export { default as ZweitausbildungAbroadLayer } from './ZweitausbildungAbroadLayer';
+export { default as ZweitausbildungPoisLayer } from './ZweitausbildungPoisLayer';
+export { default as ZweitausbildungRoutesHighlightLayer } from './ZweitausbildungRoutesHighlightLayer';
+export { default as ZweitausbildungRoutesLayer } from './ZweitausbildungRoutesLayer';
+
 export default {
   AusbauLayer,
   CasaLayer,
--- src/searches/index.js
+++ src/searches/index.js
@@ -1,3 +1,11 @@
+import Betriebspunkte from './Betriebspunkte';
+import HandicapStopFinder from './HandicapStopFinder';
+import Lines from './Lines';
+import Locations from './Locations';
+import Municipalities from './Municipalities';
+import StopFinder from './StopFinder';
+import Search from './Search';
+
 export { default as Betriebspunkte } from './Betriebspunkte';
 export { default as HandicapStopFinder } from './HandicapStopFinder';
 export { default as Lines } from './Lines';
@@ -5,3 +13,13 @@ export { default as Locations } from './Locations';
 export { default as Municipalities } from './Municipalities';
 export { default as StopFinder } from './StopFinder';
 export { default as Search } from './Search';
+
+export default {
+  Betriebspunkte,
+  HandicapStopFinder,
+  Lines,
+  Locations,
+  Municipalities,
+  StopFinder,
+  Search,
+};
