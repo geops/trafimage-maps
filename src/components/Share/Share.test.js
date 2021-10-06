7bc46eeb74b4049387635c030ee1bf97d255550e
Olivier Terral <olivier.terral@geops.de>
2021-10-06 12:10:43 +0200

    Fix test

--- src/components/Share/Share.test.js
+++ src/components/Share/Share.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { Map, View } from 'ol';
 import Share from '.';
-
-configure({ adapter: new Adapter() });
 
 describe('Share', () => {
   const mockStore = configureStore([thunk]);
