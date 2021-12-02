556e45e51ac0fc4638ca2d3a7d616f02628b4cae
Olivier Terral <olivier.terral@geops.de>
2021-12-02 11:44:04 +0100

    Remove apiKey

--- src/layers/TralisLayer/TralisLayer.js
+++ src/layers/TralisLayer/TralisLayer.js
@@ -4,7 +4,7 @@ class TralisLayer extends MBTTralisLayer {
   constructor(options) {
     // TODO this url should be set like others url
     super({
-      url: 'wss://tralis-tracker-api.geops.io/ws?key=5cc87b12d7c5370001c1d655112ec5c21e0f441792cfc2fafe3e7a1e',
+      url: 'wss://tralis-tracker-api.geops.io/ws',
       tenant: 'sbb',
       ...options,
     });
