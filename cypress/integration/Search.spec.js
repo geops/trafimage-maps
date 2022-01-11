95422c31817ea3c8b72d4350c4050dd9cad4f788
Olivier Terral <olivier.terral@geops.de>
2022-01-11 09:10:12 +0100

    Add cypress test for plural

---
+++ cypress/integration/Search.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+
+describe('Search', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+  it(`should display plural texts`, () => {
+    // Input is not visible
+    cy.get('.wkp-search-toggle-button').click();
+    cy.get('.wkp-search-input input').focus().type('Bern');
+    cy.get('[data-cy=wkp-search-section-title]').should(
+      'contain',
+      'Ergebnisse', // plural
+    );
+  });
+});
