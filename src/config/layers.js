6a76ab14ce95e40cf125f1be38e3d703aec63a78
oterral <oterral@users.noreply.github.com>
2022-02-01 16:01:28 +0100

    Use mbt@1.6.5 and react-spatial@1.1.0 (#972)
    * Use mbt@1.6.4 and react-spatial@1.1.0
    
    * Use mbt@1.6.4 and react-spatial@1.1.0
    
    * Don't use generalization levels and use mbt@1.6.5
    
    * Remove use of generalization levels for now

--- src/config/layers.js
+++ src/config/layers.js
@@ -272,6 +272,7 @@ punctuality.children = [
     useDelayStyle: true,
     tenant: 'sbb',
     minZoomNonTrain: 14,
+    generalizationLevelByZoom: [],
     regexPublishedLineName: '^(S|R$|RE|PE|D|IRE|RB|TER)',
     properties: {
       radioGroup: 'ch.sbb.punctuality',
@@ -284,6 +285,7 @@ punctuality.children = [
     useDelayStyle: true,
     tenant: 'sbb',
     minZoomNonTrain: 14,
+    generalizationLevelByZoom: [],
     regexPublishedLineName: '(IR|IC|EC|RJX|TGV)',
     properties: {
       radioGroup: 'ch.sbb.punctuality',
@@ -296,6 +298,7 @@ punctuality.children = [
     useDelayStyle: true,
     tenant: 'sbb',
     minZoomNonTrain: 14,
+    generalizationLevelByZoom: [],
     properties: {
       radioGroup: 'ch.sbb.punctuality',
     },
