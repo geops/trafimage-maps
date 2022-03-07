92f3a009f9a22c82d69878449fa5c446c34be9dc
Olivier Terral <olivier.terral@geops.de>
2022-03-07 12:19:09 +0100

    Add test for NoMouseWheelWarning component

--- src/components/NoMouseWheelWarning/NoMouseWheelWarning.test.js
+++ src/components/NoMouseWheelWarning/NoMouseWheelWarning.test.js
@@ -1,7 +1,38 @@
-// import NoMouseWheelWarning from '.';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { Map } from 'ol';
+import { MouseWheelZoom } from 'ol/interaction';
+import NoMouseWheelWarning from './NoMouseWheelWarning';
 
 describe('NoMouseWheelWarning', () => {
-  test('display warning message', () => {
-    expect(true).toBe(true);
+  test('deactivate mouse wheel interaction in embedded mode', () => {
+    const map = new Map({});
+    const mouseWheelZoom = map
+      .getInteractions()
+      .getArray()
+      .find((int) => int instanceof MouseWheelZoom);
+    expect(mouseWheelZoom.getActive()).toBe(true);
+    render(
+      <Provider store={global.mockStore({ app: { map, embedded: true } })}>
+        <NoMouseWheelWarning />
+      </Provider>,
+    );
+    expect(mouseWheelZoom.getActive()).toBe(false);
+  });
+
+  test("doesn't deactivate mouse wheel interaction", () => {
+    const map = new Map({});
+    const mouseWheelZoom = map
+      .getInteractions()
+      .getArray()
+      .find((int) => int instanceof MouseWheelZoom);
+    expect(mouseWheelZoom.getActive()).toBe(true);
+    render(
+      <Provider store={global.mockStore({ app: { map, embedded: false } })}>
+        <NoMouseWheelWarning />
+      </Provider>,
+    );
+    expect(mouseWheelZoom.getActive()).toBe(true);
   });
 });
