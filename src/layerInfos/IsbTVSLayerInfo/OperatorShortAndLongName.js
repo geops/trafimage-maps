cee95153c12aa9935a699f50ceeb1dbbba59c315
Olivier Terral <olivier.terral@geops.de>
2022-03-18 14:12:55 +0100

    Change layer name

---
+++ src/layerInfos/IsbTVSLayerInfo/OperatorShortAndLongName.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: '0.8em',
+    margin: '3px 0',
+  },
+  shortName: {
+    display: 'inline-block',
+    flex: '0 0 40px',
+    minWidth: '40px',
+    marginRight: 5,
+  },
+});
+
+const propTypes = {
+  shortName: PropTypes.string.isRequired,
+  longName: PropTypes.string.isRequired,
+};
+
+const OperatorShortAndLongName = ({ shortName, longName }) => {
+  const { t } = useTranslation();
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <span className={classes.shortName}>{shortName}</span>
+      <span> {t(longName)}</span>
+    </div>
+  );
+};
+
+OperatorShortAndLongName.propTypes = propTypes;
+
+export default React.memo(OperatorShortAndLongName);
---
+++ src/layerInfos/IsbTopicInfo/IsbTopicInfo.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const translations = {
+  de: {
+    title: 'Infrastrukturbetreiberinnen',
+    description:
+      'Die Karte der Normalspur-Infrastrukturbetreiberinnen zeigt farblich unterscheidbar deren geographische Ausbreitung. Beim Anklicken einer Strecke öffnen sich weitere Informationen, insbesondere zum Netzzugang als Eisenbahnverkehrsunternehmen.',
+    responsible: 'Verantwortlich',
+    responsibleContent: 'SBB Infrastruktur - Netzzugang',
+  },
+  fr: {
+    title: "Gestionnaires d'infrastructure",
+    description:
+      "La carte des gestionnaires d'infrastructure à voie normale montre, par des couleurs distinctes, leur extension géographique. En cliquant sur une ligne, des informations supplémentaires s'ouvrent, notamment sur l'accès au réseau en tant qu'entreprise de transport ferroviaire.",
+    responsible: 'Responsable',
+    responsibleContent: 'CFF Infrastructure - Accès au réseau',
+  },
+  en: {
+    title: 'Infrastructure managers',
+    description:
+      'The map of the standard gauge infrastructure managers shows their geographical spread in different colours. Clicking on a route opens further information, in particular on network access as a railway undertaking.',
+    responsible: 'Responsible',
+    responsibleContent: 'SBB Infrastructure - Network Access',
+  },
+  it: {
+    title: 'Gestori dell’infrastruttura',
+    description:
+      "La cartina dei gestori di infrastrutture a scartamento normale mostra la loro diffusione geografica in diversi colori. Cliccando su un percorso si aprono ulteriori informazioni, in particolare sull'accesso alla rete come impresa ferroviaria.",
+    responsible: 'Responsabile',
+    responsibleContent: 'FFS Infrastruttura - Accesso alla rete',
+  },
+};
+
+const propTypes = {
+  language: PropTypes.string.isRequired,
+};
+
+const IsbTopicInfo = ({ language }) => {
+  const { title, description, responsible, responsibleContent } =
+    translations[language];
+  return (
+    <div>
+      {title}
+      <p>{description}</p>
+      <p>
+        {responsible}:
+        <br />
+        {responsibleContent},
+        <br />
+        <a href="mailto:netzzugang@sbb.ch">netzzugang@sbb.ch</a>.
+      </p>
+    </div>
+  );
+};
+
+IsbTopicInfo.propTypes = propTypes;
+
+export default React.memo(IsbTopicInfo);
