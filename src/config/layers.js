06d6d7fc7b356a0ac589d78013b4bf991016b0c2
danji_ma90 <danji_ma90@hotmail.com>
2022-04-14 10:09:44 +0200

    Topic: Infrastrukturbetreiber (#976)
    * chore: set up boilser plate for Infrastrukturbetreiber topic
    
    * chore: using new style
    
    * chore: WIP on InfrastrukturBetreiberLayer
    
    * chore: WIP on InfrastrukturBetreiberLayer
    
    * chore: improve popup
    
    * chore: finished ISB Popup
    
    * chore: merged master into branch
    
    * v1.7.13-beta.0
    
    * chore: select feature on popup render and pagination
    
    * v1.7.13-beta.1
    
    * chore: fixed links
    
    * v1.7.20-beta.4
    
    * Add Kilometrage layer and catch properly the error when CORS is not activated
    
    * Change content of the popup
    
    * Add Topic info box
    
    * Add layer ingo for IsbTVS layer
    
    * Add layer ingo for IsbTVS layer
    
    * Add tests
    
    * Add layer info for IsbOther layer
    
    * Add test
    
    * Add SZU in not TVS layer
    
    * Change topic text
    
    * Change topic text
    
    * Add list of abbreviations
    
    * Fix color and width
    
    * Fix typo
    
    * Use default topic
    
    * Change layer name
    
    * Change layer name
    
    * Change layer name
    
    * Change layer name
    
    * Change layer name
    
    * Update list of isbs
    
    * Update list of isbs
    
    * chore: fixed tvs link in layer info
    
    * Invert bahn names
    
    * chore: update ISB popup renderTitle, fix prop errors
    
    * Add some translations
    
    * Add point
    
    * Fix en translation
    
    * Manage some case where operator has no urls and no long name
    
    * Add new lines
    
    * Add AG
    
    * Manage insensitive case
    
    * Fix name
    
    * Up browserlist
    
    Co-authored-by: danji90 <daniel.marsh-hunn@geops.de>
    Co-authored-by: Friedjoff Trautwein <friedjoff.trautwein@geops.de>
    Co-authored-by: Olivier Terral <olivier.terral@geops.de>
    Co-authored-by: oterral <oterral@users.noreply.github.com>

--- src/config/layers.js
+++ src/config/layers.js
@@ -833,10 +833,16 @@ export const netzkarteEisenbahninfrastruktur = new TrafimageMapboxLayer({
   preserveDrawingBuffer: true,
   zIndex: -1,
   style: 'netzkarte_eisenbahninfrastruktur_v3',
-  properties: {
-    hasInfos: true,
-    layerInfoComponent: 'InfrastrukturTopicInfo',
-  },
+});
+
+export const netzkarteIsb = new TrafimageMapboxLayer({
+  name: 'ch.sbb.isb',
+  isBaseLayer: true,
+  visible: true,
+  isQueryable: false,
+  preserveDrawingBuffer: true,
+  zIndex: -1,
+  style: 'netzkarte_eisenbahninfrastruktur_v3_ch.sbb.infrastrukturbetreiber',
 });
 
 export const betriebsRegionen = new MapboxStyleLayer({
@@ -1635,6 +1641,88 @@ export const direktverbindungenLayer = new Layer({
   properties: {
     hasInfos: true,
     layerInfoComponent: 'DirektVerbindungenLayerInfo',
+  },
+});
+
+// Order is important for the legend
+// WARNING: use UPPER CASE for the key
+const shortToLongNameOther = {
+  AB: 'Appenzeller Bahnen',
+  asm: 'Aare Seeland mobil AG',
+  AVA: 'Aargau Verkehr AG',
+  DB: 'Deutsche Bahn AG',
+  ÖBB: 'Österreichische Bundesbahnen',
+  RB: 'Rigi Bahnen',
+  RBS: 'Regionalverkehr Bern-Solothurn AG',
+  RhB: 'Rhätische Bahn AG',
+  SEHR: 'Stein am Rhein-Etzwilen-Hemishofen-Ramsen-Bahn',
+  SZU: 'Sihltal Zürich Uetliberg Bahn SZU AG',
+  TL: 'Transports publics de la région lausannoise',
+  VVT: 'Vapeur Val-de-Travers',
+  zb: 'Zentralbahn AG',
+};
+
+export const isbOther = new MapboxStyleLayer({
+  name: 'ch.sbb.isb.other',
+  mapboxLayer: netzkarteIsb,
+  styleLayersFilter: ({ metadata }) =>
+    metadata &&
+    metadata['isb.filter'] &&
+    /^(other|other_flag)$/.test(metadata['isb.filter']),
+  queryRenderedLayersFilter: ({ metadata }) =>
+    metadata &&
+    metadata['isb.filter'] &&
+    /^other$/.test(metadata['isb.filter']),
+  properties: {
+    hasInfos: true,
+    useOverlay: true,
+    popupComponent: 'IsbPopup',
+    layerInfoComponent: 'IsbOtherLayerInfo',
+    shortToLongName: shortToLongNameOther,
+    defaultColor: 'rgba(153, 71, 241, 1)', // Must be the same as in the style.
+  },
+});
+
+// Order is important for the legend
+// WARNING: use UPPER CASE for the key
+const shortToLongNameTVS = {
+  'SBB CFF FFS': 'SBB Infrastruktur',
+  BLS: 'BLS Netz AG',
+  SOB: 'SOB AG Infrastruktur',
+  CJ: 'Compagnie des chemins de fer du Jura SA',
+  ETB: 'Emmentalbahn GmbH',
+  HBS: 'Hafenbahn Schweiz AG',
+  OeBB: 'Oensingen-Balsthal-Bahn',
+  ST: 'Sursee-Triengen-Bahn',
+  STB: 'Sensetalbahn AG',
+  SZU: 'Sihltal Zürich Uetliberg Bahn SZU AG',
+  TMR: 'Transports de Martigny et Régions SA',
+  TPF: 'Transports publics fribourgeois SA',
+  TRAVYS: 'Travys',
+  TRN: 'Transports Publics Neuchâtelois',
+};
+
+export const isbTVS = new MapboxStyleLayer({
+  name: 'ch.sbb.isb.tvs',
+  mapboxLayer: netzkarteIsb,
+  styleLayersFilter: ({ metadata }) =>
+    metadata &&
+    metadata['isb.filter'] &&
+    /^(tvs|tvs_flag)$/.test(metadata['isb.filter']),
+  queryRenderedLayersFilter: ({ metadata }) =>
+    metadata && metadata['isb.filter'] && /^tvs$/.test(metadata['isb.filter']),
+  properties: {
+    hasInfos: true,
+    useOverlay: true,
+    popupComponent: 'IsbPopup',
+    layerInfoComponent: 'IsbTVSLayerInfo',
+    shortToLongName: shortToLongNameTVS,
+    defaultColor: 'rgba(0,91,169 , 1)', // Must be the same as in the style.
+    colors: {
+      'SBB CFF FFS': 'rgba(209, 1, 7, 1)',
+      BLS: 'rgba(53,164,48, 1)',
+      SOB: 'rgba(195, 156, 54, 1)',
+    },
   },
 });
 
