5a8db0f601211ef1b3560e7ac3422fd469df1255
danji90 <daniel.marsh-hunn@geops.de>
2022-07-19 12:08:54 +0200

    chore: use base_bright_v2_legacy in Netzkarte Sandbox

--- src/config/ch.sbb.netzkarte.sandbox/index.js
+++ src/config/ch.sbb.netzkarte.sandbox/index.js
@@ -1,6 +1,19 @@
 import { Layer } from 'mobility-toolbox-js/ol';
+import TrafimageMapboxLayer from '../../layers/TrafimageMapboxLayer';
 import LevelLayer from '../../layers/LevelLayer';
-import { dataLayer, netzkarteLayer } from '../ch.sbb.netzkarte';
+import { netzkarteLayer } from '../ch.sbb.netzkarte';
+
+export const sandboxDataLayer = new TrafimageMapboxLayer({
+  name: 'ch.sbb.netzkarte.data',
+  visible: true,
+  isQueryable: false,
+  preserveDrawingBuffer: true,
+  zIndex: -1, // Add zIndex as the MapboxLayer would block tiled layers (buslines)
+  style: 'base_bright_v2_legacy',
+  properties: {
+    hideInLegend: true,
+  },
+});
 
 export const geschosseLayer = new Layer({
   name: 'ch.sbb.geschosse',
@@ -11,7 +24,7 @@ geschosseLayer.children = [-4, -3, -2, -1, 0, '2D', 1, 2, 3, 4].map((level) => {
   return new LevelLayer({
     name: `ch.sbb.geschosse${level}`,
     visible: level === '2D',
-    mapboxLayer: dataLayer,
+    mapboxLayer: sandboxDataLayer,
     isQueryable: false,
     styleLayersFilter: ({ metadata }) => metadata && metadata['geops.filter'],
     level,
@@ -22,4 +35,4 @@ geschosseLayer.children = [-4, -3, -2, -1, 0, '2D', 1, 2, 3, 4].map((level) => {
   });
 });
 
-export default [dataLayer, netzkarteLayer, geschosseLayer];
+export default [sandboxDataLayer, netzkarteLayer, geschosseLayer];
