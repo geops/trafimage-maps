bff3650dea8644e7ab3bbd3f13fbc638563dedec
Olivier Terral <olivier.terral@geops.de>
2022-07-26 10:34:29 +0200

    fix: display consent only once on development environment

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -13,6 +13,17 @@ function Head({ topics, displayConsent, domainConsentId }) {
     return null;
   }
 
+  // By request from colleagues when the domain is not the one configured
+  // on one trust we want that the site does a fake save of the user consent
+  // after click on the button. It's avoid to display the consent everytime.
+  if (/localhost:|trafimage\.geops\.ch|\.app/.test(window.location.hostname)) {
+    // Simulate that the consent has already been given.
+    if (window.localStorage.getItem('wkp.fake.consent') === 'true') {
+      return null;
+    }
+    window.localStorage.setItem('wkp.fake.consent', true);
+  }
+
   return (
     <>
       <Helmet>
