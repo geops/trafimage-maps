882374c31d406b0c16c2fe0b95089efd650f8b8b
Olivier Terral <olivier.terral@geops.de>
2022-07-27 14:14:20 +0200

    chore: sort features results using valid_ga_hta property

--- src/layers/GeltungsbereicheLayer/GeltungsbereicheLayer.js
+++ src/layers/GeltungsbereicheLayer/GeltungsbereicheLayer.js
@@ -10,10 +10,28 @@ import { MapboxStyleLayer } from 'mobility-toolbox-js/ol';
 class GeltungsbereicheLayer extends MapboxStyleLayer {
   getFeatureInfoAtCoordinate(coordinate) {
     return super.getFeatureInfoAtCoordinate(coordinate).then((data) => {
-      const featureInfo = { ...data };
+      // const sortedData = data.sort((a, b) => {
+      //   if (a.get('valid_ga_hta'))
+      // }
+      const featureInfo = {
+        ...data,
+
+        // The sort only work for geltungsbereiche non BETA topic
+        features: data.features.sort((a, b) => {
+          // Sort by highest valid_ga_hta value
+          if (a.get('valid_ga_hta') > b.get('valid_ga_hta')) {
+            return -1;
+          }
+          if (a.get('valid_ga_hta') < b.get('valid_ga_hta')) {
+            return 1;
+          }
+          return 0;
+        }),
+      };
       if (featureInfo.features.length > 1) {
         /**
          * We make sure only unique features are selected for the popup to avoid unnecessary pagination.
+         * only geltungsbereiche BETA topic, for geltungsbreiche non BETA topic, we only show the first feature.
          * @ignore
          */
         const feats = featureInfo.features.reduce((uniques, feature) => {
