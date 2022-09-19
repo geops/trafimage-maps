3a4bbebe8bbcc46d429c49c6a3b0851e6cf85919
Olivier Terral <olivier.terral@geops.com>
2022-09-19 11:55:58 +0200

    fix(zweitausbildung): update responsible and add date of the last data update

--- src/layerInfos/ZweitausbildungTopicInfo/ZweitausbildungTopicInfo.js
+++ src/layerInfos/ZweitausbildungTopicInfo/ZweitausbildungTopicInfo.js
@@ -11,10 +11,11 @@ const ZweitausbildungTopicInfo = ({ t }) => {
   return (
     <div>
       <p>{t('ch.sbb.zweitausbildung-desc')}</p>
+      <p>{t('Letzte Aktualisierung')}: 09/2022</p>
       <p>
         {t('Verantwortlich')}:
         <br />
-        {t('HR-BIL-SKK-PM (VM und VS)')},
+        {t('HR-POK-SKK-PM (KBC und VSV)')},
         <br />
         <a href="mailto:pm.skk.kbc@sbb.ch">pm.skk.kbc@sbb.ch</a>.
       </p>
