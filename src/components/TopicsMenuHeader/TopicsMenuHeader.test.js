eac8f7835dccacef711efeb5ddc37c5e7b909343
danji_ma90 <daniel.marsh-hunn@geops.com>
2022-10-07 11:34:53 +0200

    fix: display correct subtitle in menu header (#1037)

---
+++ src/components/TopicsMenuHeader/TopicsMenuHeader.test.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import { Layer } from 'mobility-toolbox-js/ol';
+import { Provider } from 'react-redux';
+import { Map } from 'ol';
+import TopicsMenuHeader from '.';
+
+describe('TopicsMenuHeader', () => {
+  let layer1;
+  let layer2;
+  let topic1;
+  let topic2;
+  let store;
+
+  beforeEach(() => {
+    layer1 = new Layer({
+      name: 'parent1',
+      children: [
+        new Layer({
+          name: 'child',
+        }),
+        new Layer({
+          name: 'child2',
+        }),
+      ],
+    });
+
+    layer2 = new Layer({
+      name: 'layer2',
+    });
+
+    topic1 = {
+      name: 'topic1',
+      key: 'topic1',
+      layers: [layer1],
+    };
+
+    topic2 = {
+      name: 'topic2',
+      key: 'topic2',
+      layers: [layer1, layer2],
+    };
+    store = global.mockStore({
+      map: {
+        layers: topic2.layers,
+      },
+      app: {
+        menuOpen: true,
+        topics: [topic1, topic2],
+        activeTopic: topic2,
+        map: new Map(),
+      },
+    });
+  });
+
+  it('renders the active topic name and active layers', () => {
+    render(
+      <Provider store={store}>
+        <TopicsMenuHeader />
+      </Provider>,
+    );
+    expect(screen.getByText('topic2')).toBeInTheDocument();
+    expect(screen.getByText('alle aktiviert')).toBeInTheDocument();
+  });
+
+  it('updates the active layers when a visiblity changes', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <TopicsMenuHeader />
+      </Provider>,
+    );
+    expect(screen.getByText('topic2')).toBeInTheDocument();
+    const allActiveText = screen.getByText('alle aktiviert');
+    expect(allActiveText).toBeInTheDocument();
+    act(() => {
+      topic2.layers[0].children[0].visible = false;
+      topic2.layers[0].children[1].visible = false;
+    });
+    expect(screen.getByText('layer2')).toBeInTheDocument();
+    act(() => {
+      layer2.visible = false;
+    });
+    expect(
+      container.querySelector('.wkp-menu-layers.hidden'),
+    ).toBeInTheDocument();
+  });
+
+  it('updates the activetopic changes', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <TopicsMenuHeader onToggle={() => {}} />
+      </Provider>,
+    );
+    expect(screen.getByText('topic2')).toBeInTheDocument();
+    const allActiveText = screen.getByText('alle aktiviert');
+    expect(allActiveText).toBeInTheDocument();
+    act(() => {
+      topic2.layers[0].children[0].visible = false;
+      topic2.layers[0].children[1].visible = false;
+    });
+    expect(screen.getByText('layer2')).toBeInTheDocument();
+    act(() => {
+      layer2.visible = false;
+    });
+    expect(
+      container.querySelector('.wkp-menu-layers.hidden'),
+    ).toBeInTheDocument();
+  });
+});
--- src/utils/LayerService.test.js
+++ src/utils/LayerService.test.js
@@ -61,38 +61,6 @@ describe('LayerService', () => {
     expect(layerService.getParent(child).name).toBe('1');
   });
 
-  test('should return null if no radio name is given.', () => {
-    expect(layerService.getRadioGroupLayers()).toBe(null);
-  });
-
-  test('should return radio layers.', () => {
-    expect(layerService.getRadioGroupLayers('radio').length).toBe(2);
-    expect(layerService.getRadioGroupLayers('no-radio').length).toBe(0);
-  });
-
-  // test('should toggle radio layers.', () => {
-  //   layerService.getLayer('1-1').visible = true;
-  //   expect(layerService.getLayer('1-1').visible).toBe(true);
-  //   layerService.getLayer('1-2').visible = true;
-  //   expect(layerService.getLayer('1-1').visible).toBe(false);
-  // });
-
-  // test('should toggle child layers.', () => {
-  //   layerService.getLayer('1-2').visible = true;
-  //   expect(layerService.getLayer('1-2-1').visible).toBe(true);
-  //   expect(layerService.getLayer('1-2-2').visible).toBe(true);
-  //   expect(layerService.getLayer('2').visible).toBe(true);
-  // });
-
-  // test('should call back on visibility changes.', () => {
-  //   const callback = jest.fn(() => {
-  //     return 42;
-  //   });
-  //   layerService.on('change:visible', callback);
-  //   layerService.getLayer('2').visible = true;
-  //   expect(callback.mock.calls.length).toBe(0);
-  // });
-
   test('should set children from constructor', () => {
     const layer = new Layer({
       name: 'foo',
@@ -103,55 +71,5 @@ describe('LayerService', () => {
       ],
     });
     expect(layer.children.length).toBe(1);
-  });
-
-  describe('#on() ', () => {
-    test('add a callback on event.', () => {
-      const cb = () => {};
-      layerService.on('foo', cb);
-      expect(layerService.callbacks.foo[0]).toBe(cb);
-    });
-
-    test("doesn't add twice the same callback .", () => {
-      const cb = () => {};
-      layerService.on('foo', cb);
-      expect(layerService.callbacks.foo[0]).toBe(cb);
-      layerService.on('foo', cb);
-      expect(layerService.callbacks.foo[0]).toBe(cb);
-      expect(layerService.callbacks.foo[1]).toBe(undefined);
-    });
-  });
-
-  describe('#un() ', () => {
-    test("doesn't failed if callback doesn't exists.", (done) => {
-      const cb = () => {};
-      try {
-        layerService.un('foo', cb);
-        done();
-        // eslint-disable-next-line no-empty
-      } catch (e) {}
-    });
-
-    test('remove a callback on event.', () => {
-      const cb = () => {};
-      const cb1 = () => {};
-      const cb2 = () => {};
-      layerService.on('foo', cb);
-      layerService.on('foo', cb1);
-      layerService.on('foo', cb2);
-      expect(layerService.callbacks.foo[0]).toBe(cb);
-      expect(layerService.callbacks.foo[1]).toBe(cb1);
-      expect(layerService.callbacks.foo[2]).toBe(cb2);
-      layerService.un('foo', cb1);
-      expect(layerService.callbacks.foo[0]).toBe(cb);
-      expect(layerService.callbacks.foo[1]).toBe(cb2);
-      expect(layerService.callbacks.foo[2]).toBe(undefined);
-    });
-  });
-
-  test('should unlisten keys in LayerService', () => {
-    expect(layerService.keys.length).toBe(7);
-    layerService.setLayers([]);
-    expect(layerService.keys.length).toBe(0);
   });
 });
