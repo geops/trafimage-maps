96f3cc31369ac64a9379d2e1a7fce9fcf6123b86
Olivier Terral <olivier.terral@geops.com>
2023-01-16 13:37:27 +0100

    chore: improve documentation for geltungsbereiche iframe

--- src/examples/iframe/IframeDoc.js
+++ src/examples/iframe/IframeDoc.js
@@ -158,7 +158,7 @@ export const defaultPermalinkParams = [
     name: 'embedded',
     type: 'boolean',
     comp: 'checkbox',
-    defaultValue: false,
+    defaultValue: true,
     description: () => {
       return (
         <span>
