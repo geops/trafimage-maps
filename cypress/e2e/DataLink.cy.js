46836ca0ef20f2fd09448171aa59a11614705352
danji90 <daniel.marsh-hunn@geops.com>
2023-03-14 13:09:27 +0100

    chore: add cypress test for isb datalink

--- cypress/e2e/DataLink.cy.js
+++ cypress/e2e/DataLink.cy.js
@@ -1,3 +1,13 @@
+const ignoreDataLinkError = (url) =>
+  cy.origin(url, () => {
+    cy.on('uncaught:exception', () => {
+      // Exception triggered by bad code on js when loading the targeted page
+      // It will break the test by defualt if we don't return false.
+      // So let the test continue.
+      return false;
+    });
+  });
+
 // This test use chromeWebSecurity=false, it doesn't work on firefox.
 describe('A link to data', { browser: '!firefox' }, () => {
   const openDataText = 'Diesen Datensatz beziehen (Open Data)';
@@ -8,14 +18,7 @@ describe('A link to data', { browser: '!firefox' }, () => {
   });
 
   it('is available for direktverbindung layer', () => {
-    cy.origin('https://data.sbb.ch', () => {
-      cy.on('uncaught:exception', () => {
-        // Exception triggered by bad code on js when loading the targeted page
-        // It will break the test by defualt if we don't return false.
-        // So let the test continue.
-        return false;
-      });
-    });
+    ignoreDataLinkError('https://data.sbb.ch');
     // Click info button
     cy.get(':nth-child(1) > .rs-layer-tree-item > button').click();
     cy.get(
@@ -35,14 +38,7 @@ describe('A link to data', { browser: '!firefox' }, () => {
   });
 
   it('is available for bahnhofplaene layer', () => {
-    cy.origin('https://data.sbb.ch', () => {
-      cy.on('uncaught:exception', () => {
-        // Exception triggered by bad code on js when loading the targeted page
-        // It will break the test by defualt if we don't return false.
-        // So let the test continue.
-        return false;
-      });
-    });
+    ignoreDataLinkError('https://data.sbb.ch');
     // Click info button
     cy.get(':nth-child(2) > .rs-layer-tree-item > button').click();
     cy.get(
@@ -62,14 +58,7 @@ describe('A link to data', { browser: '!firefox' }, () => {
   });
 
   it('is available for passagierfrequenzen layer', () => {
-    cy.origin('https://reporting.sbb.ch', () => {
-      cy.on('uncaught:exception', () => {
-        // Exception triggered by bad code on js when loading the targeted page
-        // It will break the test by defualt if we don't return false.
-        // So let the test continue.
-        return false;
-      });
-    });
+    ignoreDataLinkError('https://reporting.sbb.ch');
 
     // Click info button
     cy.get(':nth-child(5) > .rs-layer-tree-item > button').click();
@@ -87,14 +76,7 @@ describe('A link to data', { browser: '!firefox' }, () => {
   });
 
   it('is available for construction topic', () => {
-    cy.origin('https://data.sbb.ch', () => {
-      cy.on('uncaught:exception', () => {
-        // Exception triggered by bad code on js when loading the targeted page
-        // It will break the test by defualt if we don't return false.
-        // So let the test continue.
-        return false;
-      });
-    });
+    ignoreDataLinkError('https://data.sbb.ch');
     // Click info button
     cy.get(
       ':nth-child(4) > .wkp-topic-menu-item-wrapper > .wkp-topic-icons > button',
@@ -112,6 +94,28 @@ describe('A link to data', { browser: '!firefox' }, () => {
     cy.url().should(
       'equal',
       'https://data.sbb.ch/explore/dataset/construction-projects/information/',
+    );
+  });
+
+  it('is available for isb topic', () => {
+    ignoreDataLinkError('https://data.sbb.ch');
+    // Click info button
+    cy.get(
+      ':nth-child(8) > .wkp-topic-menu-item-wrapper > .wkp-topic-icons > button',
+    ).click();
+    cy.get(
+      'a[href="https://data.sbb.ch/explore/dataset/infrastrukturbetreiberinnen/information/"]',
+    )
+      .should(([a]) => {
+        expect(a.textContent).to.equal(openDataText);
+        expect(a.target).to.equal('_blank');
+        // eslint-disable-next-line no-param-reassign
+        a.target = '_self';
+      })
+      .click();
+    cy.url().should(
+      'equal',
+      'https://data.sbb.ch/explore/dataset/infrastrukturbetreiberinnen/information/',
     );
   });
 
