9f51845d62c3b2818f0a34438384a799bf430a5c
oterral <oterral@users.noreply.github.com>
2023-03-31 14:35:43 +0200

    feat: add rokas poi background layer in sandbox topic (#1076)
    * feat: add rokas poi background layer in sandbox topic
    
    * chore: use node version for .nvmrc
    
    * chore: store the filters by style

--- src/config/ch.sbb.netzkarte.sandbox/index.js
+++ src/config/ch.sbb.netzkarte.sandbox/index.js
@@ -1,6 +1,23 @@
 import { Layer } from 'mobility-toolbox-js/ol';
+import MapboxStyleLayer from '../../layers/MapboxStyleLayer';
 import LevelLayer from '../../layers/LevelLayer';
 import { dataLayer, netzkarteLayer } from '../ch.sbb.netzkarte';
+import netzkarte from '../../img/netzkarte.png';
+
+export const poiLayer = new MapboxStyleLayer({
+  name: 'ROKAS-POIs',
+  key: 'ch.sbb.poi',
+  group: 'baseLayer',
+  properties: {
+    previewImage: netzkarte,
+    isBaseLayer: true,
+  },
+  visible: false,
+  mapboxLayer: dataLayer,
+  styleLayersFilter: ({ metadata }) =>
+    !!metadata && metadata['trafimage.filter'] === 'perimeter_mask',
+  style: 'base_bright_v2_poi',
+});
 
 export const geschosseLayer = new Layer({
   name: 'ch.sbb.geschosse',
@@ -21,4 +38,4 @@ geschosseLayer.children = [-4, -3, -2, -1, 0, '2D', 1, 2, 3, 4].map((level) => {
   });
 });
 
-export default [dataLayer, netzkarteLayer, geschosseLayer];
+export default [dataLayer, netzkarteLayer, poiLayer, geschosseLayer];
