1b050d078e6ea76a6d080fa6e8199d11363719d2
danji90 <daniel.marsh-hunn@geops.com>
2023-06-14 10:53:24 +0200

    chore: clean

--- src/popups/EnergiePopup/EnergiePopup.js
+++ src/popups/EnergiePopup/EnergiePopup.js
@@ -341,7 +341,7 @@ const EnergiePopup = ({ feature }) => {
 
   return (
     <div>
-      {!(permissionInfos?.user && activeTopic.permission === 'sbb') ? (
+      {permissionInfos?.user && activeTopic.permission === 'sbb' ? (
         <>
           <Tabs
             value={TABS.indexOf(tab)}
