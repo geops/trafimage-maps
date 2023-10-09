1b754006c6eba72d3bb5eb7c5aed8d66711189b2
danji90 <daniel.marsh-hunn@geops.com>
2023-10-09 16:36:02 +0200

    fix: update Geltungsbereiche layer info translations

--- src/layerInfos/GeltungsbereicheTopicInfo/GeltungsbereicheTopicInfo.js
+++ src/layerInfos/GeltungsbereicheTopicInfo/GeltungsbereicheTopicInfo.js
@@ -4,39 +4,46 @@ import { useTranslation } from 'react-i18next';
 const comps = {
   de: (
     <div>
-      Diese Karte zeigt die Geltungsbereiche verschiedener Abo-Produkte auf dem
-      Schweizer öV-Netz. Die Daten sind der NOVA-Datenbank entnommen. Die Karte
-      befindet sich in einem experimentellen Status. Die grafische Darstellung
-      sowie die Bedienung der Webkarte sind noch nicht optimiert. Die
-      abgebildeten Daten sind nicht verbindlich.
+      Diese Karte zeigt die aktuellen Geltungsbereiche der verschiedenen
+      Abonnemente für den öffentlichen Verkehr (öV) in der Schweiz. Die Daten
+      werden aus der öV-Vertriebsdatenbank entnommen und für diese Ansicht
+      aufbereitet. Die Aktualisierung der Karte erfolgt zweimal pro Jahr (Juni
+      und Dezember). Die Karte ist seit Anfang 2023 öffentlich zugänglich und
+      auf einigen Webseiten von beteiligten Transportunternehmen integriert.
+      Alle Angaben ohne Gewähr.
     </div>
   ),
   fr: (
     <div>
-      Cette carte présente les zones de validité des différents produits
-      d&apos;abonnement sur le réseau suisse des transports publics. Les données
-      sont extraites de la base de données NOVA. La carte est dans un état
-      expérimental. L&apos;affichage graphique et le fonctionnement de la carte
-      Web n&apos;ont pas encore été optimisés. Les données présentées ne sont
-      pas contractuelles.
+      Cette carte montre les rayons de validité actuels des différents
+      abonnements de transports publics en Suisse. Les données sont extraites de
+      la base de données de distribution des transports publics et traitées pour
+      constituer cet affichage. La carte est mise à jour deux fois par an (en
+      juin et en décembre). La carte est en libre accès depuis début 2023 et
+      intégrée aux sites Internet de certaines entreprises de transport
+      participantes. Informations fournies sans garantie.
     </div>
   ),
   it: (
     <div>
-      Questa mappa mostra i campi di validità di diversi prodotti in abbonamento
-      sulla rete dei trasporti pubblici svizzeri. I dati sono presi dal database
-      NOVA. La mappa è in uno stato sperimentale. La visualizzazione grafica e
-      il funzionamento della mappa web non sono ancora stati ottimizzati. I dati
-      riportati non sono vincolanti.
+      La mappa mostra il raggio di validità dei vari abbonamenti ai trasporti
+      pubblici svizzeri. I dati provengono dalla banca dati dei trasporti
+      pubblici e sono stati elaborati per questa visualizzazione.
+      L’aggiornamento avviene due volte all’anno (giugno e dicembre). La mappa
+      viene pubblicata dall’inizio del 2023 ed è integrata in alcuni siti web
+      delle imprese di trasporto partecipanti. Tutte le informazioni si
+      intendono senza garanzia.
     </div>
   ),
   en: (
     <div>
-      This map shows the areas of validity of various subscription products on
-      the Swiss public transport network. The data are taken from the NOVA
-      database. The map is in an experimental state. The graphic display and the
-      operation of the web map have not yet been optimized. The data shown are
-      not binding.
+      This map shows the current areas of validity of the various public
+      transport travelcards in Switzerland. The data is taken from the public
+      transport sales database and then prepared for this form of presentation.
+      The map is updated twice a year (June and December). The map has been
+      publicly accessible since the beginning of 2023 and has been integrated
+      into the websites of some participating transport companies. No liability
+      is taken for the accuracy of this information.
     </div>
   ),
 };
