54e30203583cb1bb51e9f014392e2ee27f1475b1
danji90 <daniel.marsh-hunn@geops.com>
2023-10-24 17:05:33 +0200

    chore: use different download icon

--- src/config/ch.railplus.mitglieder/RailplusExportButton.js
+++ src/config/ch.railplus.mitglieder/RailplusExportButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { FaDownload } from 'react-icons/fa';
+import { BsDownload } from 'react-icons/bs';
 import { useTranslation } from 'react-i18next';
 import MapButton from '../../components/MapButton';
 import ExportButton from '../../components/ExportButton/ExportButton';
 import { ReactComponent as Loader } from '../../img/loader.svg';
 import { RAILPLUS_EXPORTBTN_ID } from '../../utils/constants';
 
-const BtnCmpt = ({ children = <FaDownload />, title }) => {
+const BtnCmpt = ({ children, title }) => {
   const { t } = useTranslation();
   return (
     <MapButton style={{ padding: 8, color: '#444' }} title={t(title)}>
@@ -22,7 +22,7 @@ BtnCmpt.propTypes = {
 };
 
 BtnCmpt.defaultProps = {
-  children: <FaDownload />,
+  children: <BsDownload />,
   title: 'Grossformatiges PDF exportieren',
 };
 
