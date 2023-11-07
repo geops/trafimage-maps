755ba095069dee7db19a9f1e40501178353392a9
danji90 <daniel.marsh-hunn@geops.com>
2023-11-07 13:10:46 +0100

    chore: adjust iframe code in examples

--- src/examples/iframe/getIframeCodeFromUrl.js
+++ src/examples/iframe/getIframeCodeFromUrl.js
@@ -2,7 +2,7 @@ const getIframeCodeFromUrl = (url) => {
   if (!url) {
     return null;
   }
-  return `<iframe src="${url}" style="width:100%;height:100%;" />`;
+  return `<iframe src="${url}" style="width:100%;height:100%;"></iframe>`;
 };
 
 export default getIframeCodeFromUrl;
