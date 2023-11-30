3ab87c785b4b19af633552dde5ea13e36862aff6
Olivier Terral <olivier.terral@geops.com>
2023-11-30 09:18:27 +0100

    build: lint

---
+++ .stylelintrc.js
@@ -0,0 +1,31 @@
+const stylelint = {
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss"
+  ],
+  "rules": {
+    "import-notation": "string",
+    "scss/at-import-partial-extension": "always",
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        "ignorePseudoClasses": [
+          "export"
+        ]
+      }
+    ],
+    "property-no-unknown": [
+      true,
+      {
+        "ignoreProperties": [
+          "/transitiondelay/"
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = stylelint;
