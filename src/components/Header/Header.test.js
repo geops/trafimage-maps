1c93002d12fccd43961cfcd251eedb4d65c89db2
Olivier Terral <olivier.terral@geops.com>
2023-11-30 09:36:33 +0100

    build: remove react-test-renderer

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,14 +1,26 @@
 import React from "react";
-import ShallowRenderer from "react-test-renderer/shallow";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { render } from "@testing-library/react";
+import { ThemeProvider } from "@mui/material";
+import theme from "../../themes/default";
 import Header from "./Header";
 
 describe("Header", () => {
   test("match snapshots", () => {
-    const renderer = new ShallowRenderer();
-    renderer.render(<Header />);
-    const tree = renderer.getRenderOutput();
-    expect(tree).toMatchSnapshot();
-    // Login & LanguageSelect are snapshoted as 'UNDEFINED' due to React.Memo
-    // It will be solved in jest.v25: https://github.com/facebook/jest/issues/9216
+    const mockStore = configureStore([thunk]);
+    const store = mockStore({
+      map: {},
+      app: { language: "fr" },
+    });
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Header />
+        </Provider>
+      </ThemeProvider>,
+    );
+    expect(container.innerHTML).toMatchSnapshot();
   });
 });
