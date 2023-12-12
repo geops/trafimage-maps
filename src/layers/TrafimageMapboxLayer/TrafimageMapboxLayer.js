df9d24c9e94edd9cbb37f5d3b676eb3d09a78d3c
Olivier Terral <olivier.terral@geops.com>
2023-12-12 11:34:39 +0100

    fix: use prod

--- src/layers/TrafimageMapboxLayer/TrafimageMapboxLayer.js
+++ src/layers/TrafimageMapboxLayer/TrafimageMapboxLayer.js
@@ -38,7 +38,7 @@ class TrafimageMapboxLayer extends MaplibreLayer {
     this.filters = options.filters;
 
     // TODO don't use process.env here it fails in Schulzug
-    // this.stylePrefix = process?.env?.REACT_APP_STYLE_REVIEW_PREFIX || '';
+    // this.stylePrefix = process.env.REACT_APP_STYLE_REVIEW_PREFIX || '';
     this.stylePrefix = '';
     this.style = options.style;
   }
