ca60b947a8d8fa2d653d3a17aa49b351c0b0aa33
Olivier Terral <olivier.terral@geops.com>
2024-01-12 12:18:07 +0100

    fix: put back original names of layers

---
+++ src/config/ch.sbb.handicap/index.test.js
@@ -0,0 +1,17 @@
+import { getLayersAsFlatArray } from "mobility-toolbox-js/common";
+import layers from "./index";
+
+describe("ch.sbb.handicap", () => {
+  it("should always have the same layers key for layers available via permalink", () => {
+    expect(true).toBe(true);
+    const permalinkKeys = getLayersAsFlatArray(layers)
+      .filter((l) => {
+        return !l.get("hideInLegend") && !l.get("isBaseLayer");
+      })
+      .map((l) => l.key)
+      .join(",");
+    expect(permalinkKeys).toBe(
+      "ch.sbb.bahnhofplaene,ch.sbb.bahnhofplaene.interaktiv,ch.sbb.bahnhofplaene.printprodukte,ch.sbb.status_unbekannt,ch.sbb.nichtbarrierfreierbahnhoefe,ch.sbb.barrierfreierbahnhoefe",
+    );
+  });
+});
