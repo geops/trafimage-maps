a48a39463e23219b7b8d29988c0b688da4cc4331
danji90 <daniel.marsh-hunn@geops.com>
2024-02-21 15:09:07 +0100

    chore: update test description

--- src/img/geltungsbereicheLegends/index.test.js
+++ src/img/geltungsbereicheLegends/index.test.js
@@ -3,9 +3,9 @@ import geltungsbereicheLegendConfigs from ".";
 const fs = require("fs");
 const path = require("path");
 
-describe("Geltungsbereiche legend", () => {
+describe("Geltungsbereiche legends: ", () => {
   geltungsbereicheLegendConfigs.forEach((item) => {
-    it("should contain a scaleline tag for dynamic scaleline placement", async () => {
+    it(`${item.legend} should contain a scaleline tag for dynamic scaleline placement`, () => {
       const svgPath = path.join(__dirname, "./", item.legend);
       const fdr = fs.readFileSync(svgPath, "utf8", (err, data) => {
         return data;
