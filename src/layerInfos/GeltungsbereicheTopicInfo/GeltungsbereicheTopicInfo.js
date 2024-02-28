eda0aabb88937ead4af17b84780c7933af1d70fe
gevos <eva.schiller@geops.com>
2024-02-28 08:58:39 +0100

    fix: update data status (#1148)
    * fix: update data status
    
    * fix: improve styling
    
    * fix: add colon
    
    * fix: update data status II
    
    * fix: update data status III
    
    * fix: update data status IV
    
    * fix: reduce div
    
    * fix: reduce div II
    
    * fix: use different translation

--- src/layerInfos/GeltungsbereicheTopicInfo/GeltungsbereicheTopicInfo.js
+++ src/layerInfos/GeltungsbereicheTopicInfo/GeltungsbereicheTopicInfo.js
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 const comps = {
   de: (
-    <div>
+    <>
       <Typography paragraph>
         Diese Karte zeigt die aktuellen Geltungsbereiche der verschiedenen
         Abonnemente für den öffentlichen Verkehr (öV) in der Schweiz. Die Daten
@@ -14,11 +14,11 @@ const comps = {
         auf einigen Webseiten von beteiligten Transportunternehmen integriert.
         Alle Angaben ohne Gewähr.
       </Typography>
-      <Typography>Datenstand Januar 2024.</Typography>
-    </div>
+      <Typography>Datenstand Januar 2024</Typography>
+    </>
   ),
   fr: (
-    <div>
+    <>
       <Typography paragraph>
         Cette carte montre les rayons de validité actuels des différents
         abonnements de transports publics en Suisse. Les données sont extraites
@@ -28,11 +28,11 @@ const comps = {
         et intégrée aux sites Internet de certaines entreprises de transport
         participantes. Informations fournies sans garantie.
       </Typography>
-      <Typography>Mise à jour des données janvier 2024.</Typography>
-    </div>
+      <Typography>État des données Janvier 2024</Typography>
+    </>
   ),
   it: (
-    <div>
+    <>
       <Typography paragraph>
         La mappa mostra il raggio di validità dei vari abbonamenti ai trasporti
         pubblici svizzeri. I dati provengono dalla banca dati dei trasporti
@@ -42,11 +42,11 @@ const comps = {
         delle imprese di trasporto partecipanti. Tutte le informazioni si
         intendono senza garanzia.
       </Typography>
-      <Typography>Stato dei dati gennaio 2024</Typography>
-    </div>
+      <Typography>Aggiornamento dei dati gennaio 2024</Typography>
+    </>
   ),
   en: (
-    <div>
+    <>
       <Typography paragraph>
         This map shows the current areas of validity of the various public
         transport travelcards in Switzerland. The data is taken from the public
@@ -56,18 +56,14 @@ const comps = {
         been integrated into the websites of some participating transport
         companies. No liability is taken for the accuracy of this information.
       </Typography>
-      <Typography>Data status January 2024.</Typography>
-    </div>
+      <Typography>Data status January 2024</Typography>
+    </>
   ),
 };
 
 function GeltungsbereicheTopicInfo() {
   const { i18n } = useTranslation();
-  return (
-    <div>
-      <div>{comps[i18n.language]}</div>
-    </div>
-  );
+  return <div>{comps[i18n.language]}</div>;
 }
 
 export default GeltungsbereicheTopicInfo;
