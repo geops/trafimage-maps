56b10fe46569e316d6ee37dc8ce2d2c742f07d88
Olivier Terral <olivier.terral@geops.com>
2024-04-02 10:21:27 +0200

    chore: move highlight/select state management from mbt to trafimagee-maps

--- src/layers/MapboxStyleLayer/MapboxStyleLayer.js
+++ src/layers/MapboxStyleLayer/MapboxStyleLayer.js
@@ -64,6 +64,81 @@ class MapboxStyleLayer extends MTMapboxStyleLayer {
   }
 
   /**
+   * Set the hover state to true/false to a list of features.
+   * @param {Array<ol/Feature~Feature>} features
+   * @param {boolean} state Is the feature hovered
+   * @private
+   */
+  setHoverState(features, state) {
+    if (!this.mapboxLayer?.mbMap) {
+      return;
+    }
+    const { mbMap } = this.mapboxLayer;
+
+    if (!features || !mbMap) {
+      return;
+    }
+
+    features.forEach((feature) => {
+      const { source, sourceLayer } = feature.get("mapboxFeature") || {};
+      if ((!source && !sourceLayer) || !feature.getId()) {
+        if (!feature.getId()) {
+          // eslint-disable-next-line no-console
+          console.warn(
+            "No feature's id found. To use the feature state functionnality, tiles must be generated with --generate-ids. See https://github.com/mapbox/tippecanoe#adding-calculated-attributes.",
+            feature.getId(),
+            feature.getProperties(),
+          );
+        }
+        return;
+      }
+
+      mbMap.setFeatureState(
+        {
+          id: feature.getId(),
+          source,
+          sourceLayer,
+        },
+        state,
+      );
+    });
+  }
+
+  /**
+   * Select a list of features, setting the hover state to true to a list of features.
+   * @param {Array<ol/Feature~Feature>} [features=[]] Features to select.
+   * @private
+   */
+  select(features = []) {
+    this.setHoverState(this.selectedFeatures || [], false);
+    this.selectedFeatures = features;
+    this.setHoverState(this.selectedFeatures || [], true);
+  }
+
+  /**
+   * Highlight a list of features, setting the hover state to true to a list of features.
+   * @param {Array<ol/Feature~Feature>} [features=[]] Features to highlight.
+   * @private
+   */
+  highlight(features = []) {
+    // Filter out selected features
+    const filtered =
+      this.highlightedFeatures?.filter(
+        (feature) =>
+          !(this.selectedFeatures || [])
+            .map((feat) => feat.getId())
+            .includes(feature.getId()),
+      ) || [];
+
+    // Remove previous highlight
+    this.setHoverState(filtered, false);
+    this.highlightedFeatures = features;
+
+    // Add highlight
+    this.setHoverState(this.highlightedFeatures, true);
+  }
+
+  /**
    * Create exact copy of the MapboxLayer
    * @returns {MapboxLayer} MapboxLayer
    */
