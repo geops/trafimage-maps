04cd9950972fe1d6e4028c47433bcd8a8e6782d7
Olivier Terral <olivier.terral@geops.com>
2024-04-05 15:02:20 +0200

    fix: use sx

--- src/components/CloseButton/CloseButton.js
+++ src/components/CloseButton/CloseButton.js
@@ -1,25 +1,19 @@
 import PropTypes from "prop-types";
 import { IconButton } from "@mui/material";
 import React from "react";
-import { makeStyles } from "@mui/styles";
 import { MdClose } from "react-icons/md";
 import { useTranslation } from "react-i18next";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    height: 50,
-    width: 50,
-    zIndex: 1002,
-  },
-}));
-
 function CloseButton({ children, ...props }) {
   const { t } = useTranslation();
-  const classes = useStyles();
   return (
     <IconButton
       size="medium"
-      className={classes.root}
+      sx={{
+        height: 50,
+        width: 50,
+        zIndex: 1002,
+      }}
       title={t("Schliessen")}
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
