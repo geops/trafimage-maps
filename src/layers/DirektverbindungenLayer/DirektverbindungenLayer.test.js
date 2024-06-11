c60da846a88898fe8736195e6e6ceb7301ab36dd
Olivier Terral <olivier.terral@geops.com>
2024-06-11 09:03:46 +0200

    test: add test for direktverbiundung layer

---
+++ src/layers/DirektverbindungenLayer/DirektverbindungenLayer.test.js
@@ -0,0 +1,23 @@
+import { Layer } from "mobility-toolbox-js/ol";
+import OLMap from "ol/Map";
+import DirektverbindungenLayer from "./DirektverbindungenLayer";
+
+describe("DirektverbindungenLayer", () => {
+  test("should become visible when night or day layer are hidden then day layer becomes visible", () => {
+    const map = new OLMap({});
+    const layer = new DirektverbindungenLayer({
+      visible: false,
+      properties: {
+        dayLayer: new Layer({ visible: false }),
+        nightLayer: new Layer({ visible: false }),
+      },
+    });
+
+    // Listen day/night layer events
+    layer.attachToMap(map);
+
+    expect(layer.visible).toBe(false);
+    layer.get("dayLayer").visible = true;
+    expect(layer.visible).toBe(true);
+  });
+});
