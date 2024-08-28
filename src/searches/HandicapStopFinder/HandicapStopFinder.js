2a79a3c543ff5ac13e993a624db5c343cb939522
danji90 <daniel.marsh-hunn@geops.com>
2024-08-28 11:23:02 +0200

    fix(handicap): limit station search results to 50

--- src/searches/HandicapStopFinder/HandicapStopFinder.js
+++ src/searches/HandicapStopFinder/HandicapStopFinder.js
@@ -23,7 +23,7 @@ class HandicapStopFinder extends Search {
     const { signal } = this.abortController;
 
     return fetch(
-      `${this.stopsUrl}/?&q=${encodeURIComponent(value)}&key=${this.apiKey}`,
+      `${this.stopsUrl}/?&q=${encodeURIComponent(value)}&key=${this.apiKey}&limit=50`,
       {
         signal,
       },
