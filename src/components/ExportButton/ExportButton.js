9eb13ee43cc6c0663a7bee5cb5a58427a0443bf6
Olivier Terral <olivier.terral@geops.com>
2024-10-22 11:32:09 +0200

    fix: increase raiplus pdf size

--- src/components/ExportButton/ExportButton.js
+++ src/components/ExportButton/ExportButton.js
@@ -145,7 +145,7 @@ function ExportButton({
           exportFormat,
           canvas,
           exportScale,
-          sizesByFormat[exportFormat],
+          sizesByFormat[exportFormat].map((v) => v * exportScale),
           templateValues,
           imageUrl,
           fileName,
