288889e162f688d510eca26540769ec83ac5b318
danji90 <daniel.marsh-hunn@geops.com>
2025-02-10 17:40:14 +0100

    chore: fix cypress test

--- cypress/e2e/FloorSwitcher.cy.js
+++ cypress/e2e/FloorSwitcher.cy.js
@@ -3,14 +3,11 @@ describe("FloorSwitcher", () => {
     cy.consent();
   });
 
-  it("should move to the left corresponding to the overlay width.", () => {
-    cy.visit(
-      "/?lang=de&x=950701.28&y=6003978.1&z=19.17&baselayers=ch.sbb.netzkarte,ch.sbb.netzkarte.dark,ch.sbb.netzkarte.luftbild.group,ch.sbb.netzkarte.landeskarte,ch.sbb.netzkarte.landeskarte.grau&layers=ch.sbb.geschosse2D", // Zürich HB
-    );
+  it("should display the correct number of levels and insert 2D if not already in response.", () => {
     cy.intercept(
       {
         method: "GET",
-        pathname: /walking.geops.io/,
+        url: /https:\/\/walking\.geops\.io\/availableLevels\?bbox=.*/,
       },
       {
         statusCode: 200,
@@ -33,9 +30,13 @@ describe("FloorSwitcher", () => {
         },
       },
     ).as("levels");
+    cy.visit(
+      "/?lang=de&x=950701.28&y=6003978.1&z=19.17&baselayers=ch.sbb.netzkarte,ch.sbb.netzkarte.dark,ch.sbb.netzkarte.luftbild.group,ch.sbb.netzkarte.landeskarte,ch.sbb.netzkarte.landeskarte.grau&layers=ch.sbb.geschosse2D", // Zürich HB
+    );
     cy.viewport(1440, 900);
+    cy.wait("@levels");
     cy.get('[data-testid="floor-switcher"]')
       .children()
-      .should("have.length", 5);
+      .should("have.length", 6);
   });
 });
