895dd0fb917cb5a885409dc992064eada567307e
Olivier Terral <olivier.terral@geops.com>
2026-01-27 16:36:09 +0100

    chore: fix css

--- src/model/app/reducers.js
+++ src/model/app/reducers.js
@@ -225,7 +225,6 @@ export default function app(state = getInitialState(), action) {
         vectorTilesKey: action.data,
       };
     case SET_LOGIN_URL:
-      console.log("action.data", action.data);
       if (!isValidUrl(action.data)) {
         // eslint-disable-next-line no-console
         console.warn("Invalid loginUrl:", action.data);
