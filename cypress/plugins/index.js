dda6db707fd4188eda5019e8a92b5e0967611441
oterral <oterral@users.noreply.github.com>
2026-03-12 11:53:06 +0100

    test: fix e2e (#1250)
    * test: fix e2e
    
    * test: fix e2e
    
    * test: fix e2e
    
    * test: fix e2e
    
    * test: fix e2e
    
    * test: fix e2e

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,6 +8,18 @@ const resemble = require("resemblejs");
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on) => {
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    if (browser.family === "chromium") {
+      // Force software WebGL in environments without working GPU acceleration.
+      launchOptions.args.push("--use-gl=swiftshader");
+      launchOptions.args.push("--use-angle=swiftshader");
+      launchOptions.args.push("--enable-webgl");
+      launchOptions.args.push("--ignore-gpu-blocklist");
+      launchOptions.args.push("--disable-gpu");
+    }
+
+    return launchOptions;
+  });
   on("task", {
     log(message) {
       // eslint-disable-next-line no-console
