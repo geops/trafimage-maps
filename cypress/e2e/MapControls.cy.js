48facee3408a2fe506373f5e279c2caa26466bc9
Olivier Terral <olivier.terral@geops.com>
2026-05-27 12:17:37 +0200

    test: fix e2e

--- cypress/e2e/MapControls.cy.js
+++ cypress/e2e/MapControls.cy.js
@@ -19,43 +19,69 @@ describe("MapControls", () => {
     cy.intercept(
       {
         method: "GET",
-        pathname: /(municipalities|lines|SearchServer|bps)/,
+        pathname: /municipalities/,
       },
       {
         fixture: "empty-feature-collection.json",
       },
-    ).as("others");
+    ).as("municipalities");
+    cy.intercept(
+      {
+        method: "GET",
+        pathname: /lines/,
+      },
+      {
+        fixture: "empty-feature-collection.json",
+      },
+    ).as("lines");
+    cy.intercept(
+      {
+        method: "GET",
+        pathname: /SearchServer/,
+      },
+      {
+        fixture: "empty-feature-collection.json",
+      },
+    ).as("searchserver");
+    cy.intercept(
+      {
+        method: "GET",
+        pathname: /bps/,
+      },
+      {
+        fixture: "empty-feature-collection.json",
+      },
+    ).as("bps");
     cy.viewport(1440, 900);
     cy.get('[data-testid="map-controls-wrapper"]')
       .then(($mapControlsEl) => window.getComputedStyle($mapControlsEl[0]))
       .invoke("getPropertyValue", "right")
       .should("equal", "0px");
-    cy.get(".wkp-search-input input")
-      .focus()
-      .type("Bern")
+    cy.get(".wkp-search-input input").focus().type("Ber");
+    cy.wait("@municipalities");
+    cy.wait("@lines");
+    cy.wait("@searchserver");
+    cy.wait("@bps");
+    cy.wait("@search");
+    cy.get("#react-autowhatever-1-section-0-item-0")
+      .click({ force: true })
       .then(() => {
-        cy.wait("@others");
-        cy.wait("@search");
-        cy.get("#react-autowhatever-1-section-0-item-0")
-          .click({ force: true })
-          .then(() => {
-            const overlay = cy.get(".wkp-feature-information", {
-              timeout: 40000,
-            });
-            overlay.should("be.visible");
-            overlay.then(($overlayEl) => {
-              const overlayWidth = parseInt(
-                window.getComputedStyle($overlayEl[0]).width.replace("px", ""),
-                10,
-              );
-              cy.get('[data-testid="map-controls-wrapper"]')
-                .then(($mapControlsEl) =>
-                  window.getComputedStyle($mapControlsEl[0]),
-                )
-                .invoke("getPropertyValue", "right")
-                .should("equal", `${overlayWidth}px`);
-            });
-          });
+        const overlay = cy.get(".wkp-feature-information", {
+          timeout: 40000,
+        });
+        overlay.should("be.visible");
+        overlay.then(($overlayEl) => {
+          const overlayWidth = parseInt(
+            window.getComputedStyle($overlayEl[0]).width.replace("px", ""),
+            10,
+          );
+          cy.get('[data-testid="map-controls-wrapper"]')
+            .then(($mapControlsEl) =>
+              window.getComputedStyle($mapControlsEl[0]),
+            )
+            .invoke("getPropertyValue", "right")
+            .should("equal", `${overlayWidth}px`);
+        });
       });
   });
 });
